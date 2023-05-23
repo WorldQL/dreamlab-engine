d23a94edaf7651afbbca840bdb5ac6e738c10597
Jack Baron <jackmbaron@gmail.com>
2023-05-23 19:47:02 +0100

    init package

---
+++ tsup.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'tsup'
+import type { Options } from 'tsup'
+
+export function createTsupConfig({
+  entry = ['./src/index.ts'],
+  external = [],
+  noExternal = [],
+  platform = 'node',
+  format = 'esm',
+  target = 'es2022',
+  skipNodeModulesBundle = true,
+  clean = true,
+  shims = true,
+  minify = false,
+  splitting = false,
+  keepNames = true,
+  dts = true,
+  sourcemap = true,
+}: Options = {}) {
+  return defineConfig({
+    entry,
+    external,
+    noExternal,
+    platform,
+    format,
+    skipNodeModulesBundle,
+    target,
+    clean,
+    shims,
+    minify,
+    splitting,
+    keepNames,
+    dts,
+    sourcemap,
+  })
+}
+
+export default createTsupConfig({
+  shims: false,
+})
