5da5c82e7c4a44b7895faa47c46b8c9ac81c7feb
Jack Baron <jackmbaron@gmail.com>
2023-05-26 05:23:19 +0100

    export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export {}
+export { createGame } from '~/game.js'
+export { createEntity, isEntity } from '~/entity.js'
+export type {
+  Entity,
+  PartialEntity,
+  InitContext,
+  RenderContext,
+} from '~/entity.js'
