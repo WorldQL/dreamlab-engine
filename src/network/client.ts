d757679a9dad6c6d1519de5a8718848c165fd2be
Jack Baron <jackmbaron@gmail.com>
2023-06-26 19:12:19 +0100

    implement netclient

--- src/network/client.ts
+++ src/network/client.ts
@@ -1,6 +1,37 @@
+import type { Payload } from './shared.js'
+
+export type MessageListener = (payload: Payload) => void
 export interface NetClient {
   type: 'client'
-  // TODO
+
+  send(payload: Payload): void
+
+  addMessageListener(listener: MessageListener): void
+  removeMessageListener(listener: MessageListener): void
 }
 
-export const createNetClient = (): NetClient => ({ type: 'client' })
+export const createNetClient = (ws: WebSocket): NetClient => {
+  const listeners = new Set<MessageListener>()
+  const onMessage = ({ data }: MessageEvent) => {
+    // TODO: Decode correctly
+
+    for (const fn of listeners) {
+      fn(data)
+    }
+  }
+
+  ws.addEventListener('message', onMessage)
+  const net: NetClient = {
+    type: 'client',
+
+    send: payload => {
+      // TODO: Encode correctly
+      ws.send(JSON.stringify(payload))
+    },
+
+    addMessageListener: fn => listeners.add(fn),
+    removeMessageListener: fn => listeners.delete(fn),
+  }
+
+  return Object.freeze(net)
+}
---
+++ src/network/shared.ts
@@ -0,0 +1,2 @@
+// TODO: Define a well-known wire format
+export type Payload = unknown
