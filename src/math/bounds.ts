44a5e739247041e812e16fcbbf56b1d2ac97bb59
Jack Baron <jackmbaron@gmail.com>
2023-06-27 17:50:05 +0100

    impl fast bounds check

--- src/math/bounds.ts
+++ src/math/bounds.ts
@@ -3,26 +3,40 @@ import type { Transform } from './transform.js'
 import { Vec } from '~/math/vector.js'
 import type { Vector } from '~/math/vector.js'
 
+const fastBoundsTest = (
+  width: number,
+  height: number,
+  position: Vector,
+  { x, y }: Vector,
+): boolean => {
+  const size = Vec.create(width, height)
+  const { x: minX, y: minY } = Vec.sub(position, size)
+  const { x: maxX, y: maxY } = Vec.add(position, size)
+
+  return x >= minX && x <= maxX && y >= minY && y <= maxY
+}
+
 export const simpleBoundsTest = (
   width: number,
   height: number,
   transform: Transform,
   point: Vector,
 ): boolean => {
-  // TODO: Optimise with quick bounds check
+  const fast = fastBoundsTest(width, height, transform.position, point)
+  if (!fast) return false
 
   const radians = toRadians(transform.rotation)
-  const angle_sin = Math.sin(radians)
-  const angle_cos = Math.cos(radians)
+  const sin = Math.sin(radians)
+  const cos = Math.cos(radians)
 
   const x =
-    (point.x - transform.position.x) * angle_cos -
-    (point.y - transform.position.y) * angle_sin +
+    (point.x - transform.position.x) * cos -
+    (point.y - transform.position.y) * sin +
     transform.position.x
 
   const y =
-    (point.x - transform.position.x) * angle_sin +
-    (point.y - transform.position.y) * angle_cos +
+    (point.x - transform.position.x) * sin +
+    (point.y - transform.position.y) * cos +
     transform.position.y
 
   const half = Vec.create(width / 2, height / 2)
