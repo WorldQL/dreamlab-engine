a3c91730c4351195b40c0e6f4ac9533de8aa6c98
Jack Baron <jackmbaron@gmail.com>
2023-07-05 16:42:48 +0100

    ball

---
+++ src/entities/spawnable/bouncyBall.ts
@@ -0,0 +1,102 @@
+import Matter from 'matter-js'
+import { Graphics } from 'pixi.js'
+import { toDegrees, toRadians } from '~/math/general.js'
+import { Vec } from '~/math/vector.js'
+import { createSpawnableEntity } from '~/spawnable/spawnableEntity.js'
+import { createSprite } from '~/textures/sprites.js'
+import type { SpriteSource } from '~/textures/sprites.js'
+import { drawCircle } from '~/utils/draw.js'
+
+export const createBouncyBall = createSpawnableEntity(
+  'createBouncyBall',
+  (
+    { transform, zIndex, tags, preview },
+    radius: number,
+    spriteSource?: SpriteSource,
+  ) => {
+    const { position, rotation } = transform
+
+    const mass = 20
+    const body = Matter.Bodies.circle(position.x, position.y, radius, {
+      label: 'bouncyBall',
+      render: { visible: false },
+      angle: toRadians(rotation),
+      isStatic: preview,
+      isSensor: preview,
+
+      // inertia: Number.POSITIVE_INFINITY,
+      // inverseInertia: 0,
+      mass,
+      inverseMass: 1 / mass,
+      restitution: 0.95,
+    })
+
+    return {
+      get transform() {
+        return {
+          position: Vec.clone(body.position),
+          rotation: toDegrees(body.angle),
+        }
+      },
+
+      get tags() {
+        return tags
+      },
+
+      isInBounds(position) {
+        return Matter.Query.point([body], position).length > 0
+      },
+
+      init({ game, physics }) {
+        const debug = game.debug
+        Matter.Composite.add(physics.world, body)
+
+        return { debug, physics, body }
+      },
+
+      initRenderContext(_, { stage, camera }) {
+        const gfx = new Graphics()
+        gfx.zIndex = zIndex + 1
+        drawCircle(gfx, { radius })
+
+        const width = radius * 2
+        const height = radius * 2
+        const sprite = spriteSource
+          ? createSprite(spriteSource, { width, height, zIndex })
+          : undefined
+
+        stage.addChild(gfx)
+        if (sprite) stage.addChild(sprite)
+
+        return { camera, gfx, sprite }
+      },
+
+      teardown({ physics, body }) {
+        Matter.Composite.remove(physics.world, body)
+      },
+
+      teardownRenderContext({ gfx, sprite }) {
+        gfx.removeFromParent()
+        gfx.destroy()
+
+        if (sprite) {
+          sprite.removeFromParent()
+          sprite.destroy()
+        }
+      },
+
+      onRenderFrame(_, { debug, body }, { camera, gfx, sprite }) {
+        const pos = Vec.add(body.position, camera.offset)
+
+        gfx.position = pos
+        gfx.rotation = body.angle
+        gfx.alpha = debug.value ? 0.5 : 0
+
+        if (sprite) {
+          sprite.position = pos
+          sprite.angle = rotation
+        }
+      },
+    }
+  },
+)
--- src/exports/entities.ts
+++ src/exports/entities.ts
@@ -5,6 +5,7 @@ export * from '~/entities/inputs.js'
 export * from '~/entities/netplayer.js'
 export * from '~/entities/player.js'
 
+export * from '~/entities/spawnable/bouncyBall.js'
 export * from '~/entities/spawnable/complexSolid.js'
 export * from '~/entities/spawnable/marker.js'
 export * from '~/entities/spawnable/nonsolid.js'
