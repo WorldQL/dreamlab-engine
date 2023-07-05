ea81ac5a28bcf63fb168b8df2abc99417a366003
Jack Baron <jackmbaron@gmail.com>
2023-07-05 14:35:23 +0100

    render netplayer bounds for testing

--- src/entities/netplayer.ts
+++ src/entities/netplayer.ts
@@ -1,19 +1,31 @@
 import { createId } from '@paralleldrive/cuid2'
 import Matter from 'matter-js'
 import type { Body, Engine, Vector } from 'matter-js'
+import { Graphics } from 'pixi.js'
 import { PLAYER_MASS } from './player.js'
 import type { PlayerCommon, PlayerOptions } from './player.js'
+import type { Camera } from '~/entities/camera.js'
 import { createEntity, dataManager } from '~/entity.js'
 import type { Entity } from '~/entity.js'
 import { v, Vec } from '~/math/vector.js'
 import type { LooseVector } from '~/math/vector.js'
+import type { Debug } from '~/utils/debug.js'
+import { drawBox } from '~/utils/draw.js'
 
 interface Data {
+  debug: Debug
   physics: Engine
   body: Body
 }
 
-interface NetPlayer extends PlayerCommon, Entity<Data, unknown> {
+interface Render {
+  camera: Camera
+
+  // sprite: AnimatedSprite
+  gfxBounds: Graphics
+}
+
+interface NetPlayer extends PlayerCommon, Entity<Data, Render> {
   get id(): string
   get body(): Body
 
@@ -27,7 +39,7 @@ export const createNetPlayer = ({
 }: PlayerOptions = {}) => {
   const id = createId()
 
-  const netPlayer: NetPlayer = createEntity<NetPlayer, Data, unknown>({
+  const netPlayer: NetPlayer = createEntity<NetPlayer, Data, Render>({
     get id(): string {
       return id
     },
@@ -52,7 +64,9 @@ export const createNetPlayer = ({
       return body
     },
 
-    init({ physics }) {
+    init({ game, physics }) {
+      const debug = game.debug
+
       const body = Matter.Bodies.rectangle(0, 0, width, height, {
         label: 'player',
         render: { visible: false },
@@ -66,19 +80,33 @@ export const createNetPlayer = ({
 
       Matter.Composite.add(physics.world, body)
 
-      return { physics, body }
+      return { debug, physics, body }
     },
 
-    initRenderContext() {
-      // No-op
+    initRenderContext(_, { stage, camera }) {
+      const gfxBounds = new Graphics()
+
+      gfxBounds.zIndex = 6 // Set sprite to 5 and change to "+ 1"
+      drawBox(gfxBounds, { width, height }, { stroke: '#00f' })
+      stage.addChild(gfxBounds)
+
+      return { camera, gfxBounds }
     },
 
     teardown({ physics, body }) {
       Matter.Composite.remove(physics.world, body)
     },
 
-    teardownRenderContext() {
-      // No-op
+    teardownRenderContext({ gfxBounds }) {
+      gfxBounds.removeFromParent()
+      gfxBounds.destroy()
+    },
+
+    onRenderFrame(_, { debug, body }, { camera, gfxBounds }) {
+      const pos = Vec.add(body.position, camera.offset)
+
+      gfxBounds.position = pos
+      gfxBounds.alpha = debug.value ? 0.5 : 0
     },
   })
 
