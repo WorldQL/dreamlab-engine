70ac4453d55afd9e085918c7032f00b47ff4d66f
Jack Baron <jackmbaron@gmail.com>
2023-07-27 17:22:39 +0100

    use global symbol entries

--- src/spawnable/spawnableEntity.ts
+++ src/spawnable/spawnableEntity.ts
@@ -11,7 +11,7 @@ import { registerSpawnableFn } from '~/spawnable/spawn.js'
 import { mergeObjects } from '~/utils/types.js'
 
 export type UID = string
-const symbol = Symbol('spawnable entity')
+const symbol = Symbol.for('@dreamlab/core/spawnable-entity')
 
 export interface SpawnableEntity<Data = unknown, Render = unknown>
   extends Entity<Data, Render> {
