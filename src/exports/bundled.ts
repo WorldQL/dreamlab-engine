2da58cde4274f8cac500951512d4ee0974ac5bfb
Jack Baron <jackmbaron@gmail.com>
2023-07-31 16:30:41 +0100

    add mega bundled output

---
+++ src/exports/bundled.ts
@@ -0,0 +1,7 @@
+export * from './entities.js'
+export * from './index.js'
+export * from './input.js'
+export * from './math.js'
+export * from './network.js'
+export * from './textures.js'
+export * from './utils.js'
