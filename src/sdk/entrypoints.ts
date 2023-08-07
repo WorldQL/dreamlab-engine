1ed78df6b575e679ec6a2cd95003946c3255cb0a
Jack Baron <jackmbaron@gmail.com>
2023-08-07 18:10:04 +0100

    add entrypoint types

--- src/sdk/entrypoints.ts
+++ src/sdk/entrypoints.ts
@@ -1,2 +1,9 @@
-// TODO
-export {}
+import type { Game } from '~/game.js'
+
+type InitFn<Headless extends boolean> = (
+  game: Game<Headless>,
+) => Promise<void> | void
+
+export type InitClient = InitFn<false>
+export type InitServer = InitFn<false>
+export type InitShared = InitFn<boolean>
