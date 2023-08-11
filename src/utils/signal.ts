60097c57e68e523411125548afae1900700262ed
Jack Baron <jackmbaron@gmail.com>
2023-08-11 16:39:25 +0100

    signal

---
+++ src/utils/signal.ts
@@ -0,0 +1,12 @@
+export const createSignal = <T = void>(): [
+  fn: (data: T) => void,
+  signal: Promise<T>,
+] => {
+  let fn: ((data: T) => void) | undefined
+  const promise = new Promise<T>(resolve => {
+    fn = resolve
+  })
+
+  if (fn === undefined) throw new Error('failed to assign signal fn')
+  return [fn, promise]
+}
