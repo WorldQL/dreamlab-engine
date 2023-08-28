a8a7b18a38c401c3571d5d118225f3d442f0ece3
Jack Baron <jackmbaron@gmail.com>
2023-08-28 10:18:13 +0100

    skeleton kv interface

--- src/sdk/kv.ts
+++ src/sdk/kv.ts
@@ -1,2 +1,21 @@
-// TODO
-export {}
+interface KvStore {
+  /**
+   * Get a value local to the current player
+   */
+  getPlayer(key: string): Promise<string | undefined>
+
+  /**
+   * Get a value for this world
+   */
+  getWorld(key: string): Promise<string | undefined>
+
+  /**
+   * Set a value local to the current player
+   */
+  setPlayer(key: string, value: string): Promise<void>
+
+  /**
+   * Set a value for this world
+   */
+  setWorld(key: string, value: string): Promise<void>
+}
