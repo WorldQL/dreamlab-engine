737b4d58253dbbd74d656f5104c0d5f68c54601b
Jackson Roberts <jacksonroberts25@gmail.com>
2023-09-01 21:48:15 -0600

    prettier format.

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -22,22 +22,22 @@ export const PLAYER_ANIMATION_SPEED = 0.4
 export const PLAYER_SPRITE_ANCHOR = [0.45, 0.535] as const
 
 interface PlayerDataMap {
-  [key: string]: unknown;
-};
+  [key: string]: unknown
+}
 
 export class PlayerDataManager {
-  private static playerData: PlayerDataMap = {};
+  private static playerData: PlayerDataMap = {}
 
   public static set(key: string, value: unknown) {
     this.playerData[key] = value
   }
 
   public static get(key: string): unknown {
-    return this.playerData[key];
+    return this.playerData[key]
   }
 
   public static getAll(): PlayerDataMap {
-    return this.playerData;
+    return this.playerData
   }
 }
 
