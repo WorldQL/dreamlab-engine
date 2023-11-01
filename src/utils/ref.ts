e4e331ce8b846fca65217a8568f51a890c891c25
Jack Baron <jackmbaron@gmail.com>
2023-11-01 19:41:18 +0000

    trackedref

--- src/utils/ref.ts
+++ src/utils/ref.ts
@@ -1,3 +1,42 @@
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+type Primitive = bigint | boolean | number | string | symbol | null | undefined
+
+type TrackedRefFn<T> = (value: T) => void
+type TrackedRefAugment<T> = {
+  onChanged(fn: TrackedRefFn<T>): void
+  removeListener(fn: TrackedRefFn<T>): void
+}
+
 export type Ref<T> = { value: T }
-export const ref = <T>(value: T): Ref<T> => ({ value })
+export type TrackedRef<T> = Ref<T> & TrackedRefAugment<T>
+
+export function ref<T>(value: T, track?: false): Ref<T>
+export function ref<T>(
+  value: T,
+  track: true,
+): T extends Primitive ? TrackedRef<T> : never
+export function ref<T>(value: T, track?: boolean): Ref<T> | TrackedRef<T> {
+  if (!track) return { value }
+
+  const fns = new Set<TrackedRefFn<T>>()
+  const proxy = new Proxy<Ref<T>>(
+    { value },
+    {
+      set: (target, property, value, receiver) => {
+        if (property === 'value') {
+          for (const fn of fns) fn(value)
+        }
+
+        return Reflect.set(target, property, value, receiver)
+      },
+    },
+  )
+
+  const augment: TrackedRefAugment<T> = {
+    onChanged: fn => fns.add(fn),
+    removeListener: fn => fns.delete(fn),
+  }
+
+  return Object.assign(proxy, augment)
+}
