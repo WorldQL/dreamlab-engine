4c39beb45833145cadce2eaa9f7b888887ca602d
Jack Baron <jackmbaron@gmail.com>
2023-11-09 16:02:05 +0000

    defer utils: wait for player

--- src/utils/defer.ts
+++ src/utils/defer.ts
@@ -1,4 +1,7 @@
+import { isPlayer } from '~/entities/player.js'
+import type { Player } from '~/entities/player.js'
 import type { RenderTime, Time } from '~/entity.js'
+import type { EventHandler } from '~/events.js'
 import type { Game } from '~/game.js'
 
 export const waitForPhysicsStep = async (
@@ -27,4 +30,40 @@ export const deferUntilRenderFrame = (
   fn: (time: RenderTime) => void,
 ) => {
   game.events.client.once('onRenderFrame', fn)
+}
+
+export const waitForPlayer = async (game: Game<boolean>): Promise<Player> => {
+  const player = game.entities.find(isPlayer)
+  if (player) return player
+
+  return new Promise<Player>(resolve => {
+    const onInstantiate: EventHandler<'onInstantiate'> = entity => {
+      if (isPlayer(entity)) {
+        game.events.common.removeListener('onInstantiate', onInstantiate)
+        resolve(entity)
+      }
+    }
+
+    game.events.common.addListener('onInstantiate', onInstantiate)
+  })
+}
+
+export const deferUntilPlayer = (
+  game: Game<boolean>,
+  fn: (player: Player) => void,
+) => {
+  const player = game.entities.find(isPlayer)
+  if (player) {
+    fn(player)
+    return
+  }
+
+  const onInstantiate: EventHandler<'onInstantiate'> = entity => {
+    if (isPlayer(entity)) {
+      game.events.common.removeListener('onInstantiate', onInstantiate)
+      fn(entity)
+    }
+  }
+
+  game.events.common.addListener('onInstantiate', onInstantiate)
 }
