56c10393926f2209809a9ca30b3163a55fcf203a
Jack Baron <jackmbaron@gmail.com>
2023-11-13 20:15:27 +0000

    document ui manager

--- src/ui.ts
+++ src/ui.ts
@@ -1,28 +1,51 @@
 export interface ClientUIManager {
+  /**
+   * Add a DOM node to the UI overlay
+   *
+   * @param node - HTML Node
+   */
   add(node: Node): void
+
+  /**
+   * Remove a DOM node from the UI overlay
+   *
+   * @param node - HTML Node
+   */
   remove(node: Node): void
+
+  /**
+   * Get a list of CSS Style Sheets for the UI root
+   */
   styles(): CSSStyleSheet[]
+
+  /**
+   * Enable `user-select` and `pointer-events` on a UI element
+   *
+   * @param element - HTML Element
+   */
   makeInteractable(element: HTMLElement): void
 }
 
-export function createClientUI(canvasContainer: Element): ClientUIManager {
-  const shadowRootContainer = Object.assign(document.createElement('div'), {
-    id: 'client-ui-container',
-  })
+export const createClientUI = (
+  canvasContainer: Element,
+  id = 'client-ui-container',
+): ClientUIManager => {
+  const rootElement = document.createElement('div')
+  rootElement.id = id
 
   // always on top, mouse click-through:
-  shadowRootContainer.style.pointerEvents = 'none'
-  shadowRootContainer.style.userSelect = 'none'
-  shadowRootContainer.style.position = 'absolute'
-  shadowRootContainer.style.width = '100%'
-  shadowRootContainer.style.height = '100%'
-  shadowRootContainer.style.top = '0'
-  shadowRootContainer.style.left = '0'
+  rootElement.style.pointerEvents = 'none'
+  rootElement.style.userSelect = 'none'
+  rootElement.style.position = 'absolute'
+  rootElement.style.width = '100%'
+  rootElement.style.height = '100%'
+  rootElement.style.top = '0'
+  rootElement.style.left = '0'
 
-  canvasContainer.appendChild(shadowRootContainer)
-  const shadowRoot = shadowRootContainer.attachShadow({ mode: 'closed' })
+  canvasContainer.appendChild(rootElement)
+  const shadowRoot = rootElement.attachShadow({ mode: 'closed' })
 
-  return {
+  const ui: ClientUIManager = {
     add(node) {
       shadowRoot.appendChild(node)
     },
@@ -40,4 +63,6 @@ export function createClientUI(canvasContainer: Element): ClientUIManager {
       element.style.pointerEvents = 'auto'
     },
   }
+
+  return Object.freeze(ui)
 }
