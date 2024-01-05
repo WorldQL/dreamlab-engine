23e3babac58a410a67ae26d1216800d43632d921
Jackson Roberts <jacksonroberts25@gmail.com>
2024-01-05 10:30:34 -0700

    make nonsolid api less crazy.

--- src/textures/sprites.ts
+++ src/textures/sprites.ts
@@ -1,19 +1,16 @@
 import { Sprite, Texture, TilingSprite } from 'pixi.js'
 import { z } from 'zod'
 
-export type SpriteSourceOptions = z.infer<typeof SpriteSourceOptionsSchema>
-export const SpriteSourceOptionsSchema = z.object({
-  url: z.string(),
-  tile: z.boolean().or(z.number()).optional(),
-})
-
 export type SpriteSource = z.infer<typeof SpriteSourceSchema>
-export const SpriteSourceSchema = z.string().or(SpriteSourceOptionsSchema)
+export const SpriteSourceSchema = z.object({
+  url: z.string(),
+  tile: z.boolean().default(false),
+  tileScale: z.number().optional(),
+})
 
 export interface SpriteOptions {
   width?: number
   height?: number
-
   zIndex?: number
 }
 
@@ -21,15 +18,14 @@ export const createSprite = (
   source: SpriteSource,
   { width, height, zIndex }: SpriteOptions = {},
 ): Sprite => {
-  const { url, tile = false }: SpriteSourceOptions =
-    typeof source === 'string' ? { url: source } : source
+  const { url, tile, tileScale }: SpriteSource = source
 
   const texture = Texture.from(url)
   const createSprite = () => {
     if (!tile) return new Sprite(texture)
 
     const sprite = new TilingSprite(texture)
-    if (typeof tile === 'number') sprite.tileScale.set(tile)
+    if (typeof tileScale === 'number') sprite.tileScale.set(tileScale)
 
     return sprite
   }
