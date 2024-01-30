4a354cdba43f8e1db0f1caed9c4b782bea0e3642
Jack Baron <jackmbaron@gmail.com>
2024-01-30 18:16:16 +0000

    only resolve world://

--- src/sdk/resolve.ts
+++ src/sdk/resolve.ts
@@ -1,3 +1,8 @@
 export const resolve = (url: string): string => {
+  // eslint-disable-next-line n/prefer-global/url
+  const uri = new URL(url)
+  if (uri.protocol !== 'world') return url
+
   // TODO: Grab base URL of world
+  return ''
 }
