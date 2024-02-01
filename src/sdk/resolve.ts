9b7958272a9e99a8cb47bde87e44133af01a7323
Jackson Roberts <jacksonroberts25@gmail.com>
2024-02-01 02:12:31 -0700

    improve docstring.

--- src/sdk/resolve.ts
+++ src/sdk/resolve.ts
@@ -3,7 +3,7 @@ import type { LiteralUnion } from 'type-fest'
 /**
  * Resolve a `world://` URL to a world's `public` directory
  *
- * @param url - `world://` URL
+ * @param url - `world://` URL (which refers to files in the current world's repository) or an `https://` URL for external resources
  */
 export const resolve = (url: LiteralUnion<'world://', string>): string => {
   const prefix = 'world://'
