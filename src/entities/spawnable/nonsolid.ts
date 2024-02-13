2e42bd61d764b59a5f45c412ad82698667682582
Jack Baron <jackmbaron@gmail.com>
2024-02-13 01:28:57 +0000

    port a few entities as an example

--- src/entities/spawnable/nonsolid.ts
+++ src/entities/spawnable/nonsolid.ts
@@ -1,123 +1,220 @@
-import { Container, Graphics } from 'pixi.js'
+import type { Vector } from 'matter-js'
 import type { Sprite } from 'pixi.js'
+import { Container, Graphics } from 'pixi.js'
 import { z } from 'zod'
-import type { Camera } from '~/entities/camera.js'
-import { simpleBoundsTest } from '~/math/bounds.js'
-import { Vec } from '~/math/vector.js'
-import {
-  updateSpriteSource,
-  updateSpriteWidthHeight,
-} from '~/spawnable/args.js'
-import { createSpawnableEntity } from '~/spawnable/spawnableEntity.js'
-import type { SpawnableEntity } from '~/spawnable/spawnableEntity.js'
-import { createSprite, SpriteSourceSchema } from '~/textures/sprites.js'
-import type { Debug } from '~/utils/debug.js'
-import { drawBox } from '~/utils/draw.js'
-import type { RedrawBox } from '~/utils/draw.js'
+import type { Camera } from '~/entities/camera'
+import type { InitContext, InitRenderContext, RenderTime } from '~/entity'
+import { simpleBoundsTest } from '~/math/bounds'
+import type { Bounds } from '~/math/bounds'
+import { Vec } from '~/math/vector'
+import type { ArgsPath, PreviousArgs } from '~/spawnable/spawnableEntity'
+import { SpawnableEntity } from '~/spawnable/spawnableEntity'
+import { createSprite, SpriteSourceSchema } from '~/textures/sprites'
+import type { Debug } from '~/utils/debug'
+import { drawBox } from '~/utils/draw'
+import type { RedrawBox } from '~/utils/draw'
 
 type Args = typeof ArgsSchema
-const ArgsSchema = z.object({
+export const ArgsSchema = z.object({
   width: z.number().positive().min(1).default(100),
   height: z.number().positive().min(1).default(100),
   spriteSource: SpriteSourceSchema.optional(),
 })
 
-interface Data {
-  debug: Debug
-}
+export class NonSolid extends SpawnableEntity<Args> {
+  protected readonly stroke: string = 'blue'
 
-interface Render {
-  camera: Camera
-  container: Container
-  gfx: Graphics
-  redrawGfx: RedrawBox
-  sprite: Sprite | undefined
-}
+  protected declare debug: Debug
+  protected declare camera: Camera
+  protected declare container: Container
+  protected declare gfx: Graphics
+  protected declare redrawGfx: RedrawBox
+  protected declare sprite: Sprite | undefined
 
-export const createNonsolid = createSpawnableEntity<
-  Args,
-  SpawnableEntity<Data, Render, Args>,
-  Data,
-  Render
->(ArgsSchema, ({ transform }, args) => ({
-  rectangleBounds() {
-    return { width: args.width, height: args.height }
-  },
+  public override bounds(): Bounds | undefined {
+    const { width, height } = this.args
+    return { width, height }
+  }
 
-  isPointInside(point) {
-    return simpleBoundsTest(
-      { width: args.width, height: args.height },
-      transform,
-      point,
+  public override isPointInside(point: Vector): boolean {
+    const { width, height } = this.args
+    return simpleBoundsTest({ width, height }, this.transform, point)
+  }
+
+  public override init({ game }: InitContext): void {
+    this.debug = game.debug
+  }
+
+  public override initRender({ stage, camera }: InitRenderContext): void {
+    this.camera = camera
+    const { width, height, spriteSource } = this.args
+
+    this.container = new Container()
+    this.container.sortableChildren = true
+    this.container.zIndex = this.transform.zIndex
+
+    this.gfx = new Graphics()
+    this.gfx.zIndex = 100
+    this.redrawGfx = drawBox(
+      this.gfx,
+      { width, height },
+      { stroke: this.stroke },
     )
-  },
 
-  init({ game }) {
-    return { debug: game.debug }
-  },
-
-  initRenderContext(_, { stage, camera }) {
-    const { width, height, spriteSource } = args
-
-    const container = new Container()
-    container.sortableChildren = true
-    container.zIndex = transform.zIndex
-
-    const gfx = new Graphics()
-    gfx.zIndex = 100
-    const redrawGfx = drawBox(gfx, { width, height }, { stroke: 'blue' })
-
-    const sprite = spriteSource
+    this.sprite = spriteSource
       ? createSprite(spriteSource, { width, height })
       : undefined
 
-    container.addChild(gfx)
-    if (sprite) container.addChild(sprite)
-    stage.addChild(container)
+    this.container.addChild(this.gfx)
+    if (this.sprite) this.container.addChild(this.sprite)
+    stage.addChild(this.container)
 
-    transform.addZIndexListener(() => {
-      container.zIndex = transform.zIndex
+    this.transform.addZIndexListener(() => {
+      this.container.zIndex = this.transform.zIndex
     })
+  }
 
-    return { camera, container, gfx, redrawGfx, sprite }
-  },
+  public override onArgsUpdate(
+    _path: ArgsPath<Args>,
+    _previousArgs: PreviousArgs<Args>,
+  ): void {
+    // TODO
+  }
 
-  onArgsUpdate(path, _previous, _data, render) {
-    updateSpriteWidthHeight(path, render?.sprite, args)
+  public override onResize(bounds: Bounds): void {
+    this.args.width = bounds.width
+    this.args.height = bounds.height
+  }
 
-    if (render && (path === 'width' || path === 'height')) {
-      render.redrawGfx(args)
-    }
-
-    updateSpriteSource(
-      path,
-      'sprite',
-      'spriteSource',
-      'container',
-      args,
-      render,
-    )
-  },
-
-  onResize({ width, height }) {
-    args.width = width
-    args.height = height
-  },
-
-  teardown(_) {
+  public override teardown(): void {
     // No-op
-  },
+  }
 
-  teardownRenderContext({ gfx, sprite }) {
-    gfx.destroy()
-    sprite?.destroy()
-  },
+  public override teardownRender(): void {
+    this.container.destroy({ children: true })
+  }
 
-  onRenderFrame(_, { debug }, { camera, container, gfx }) {
-    const pos = Vec.add(transform.position, camera.offset)
+  public override onRenderFrame(_time: RenderTime): void {
+    const pos = Vec.add(this.transform.position, this.camera.offset)
 
-    container.position = pos
-    container.angle = transform.rotation
-    gfx.alpha = debug.value ? 0.5 : 0
-  },
-}))
+    this.container.position = pos
+    this.container.angle = this.transform.rotation
+    this.gfx.alpha = this.debug.value ? 0.5 : 0
+  }
+}
+
+// import { Container, Graphics } from 'pixi.js'
+// import type { Sprite } from 'pixi.js'
+// import type { Camera } from '~/entities/camera.js'
+// import { simpleBoundsTest } from '~/math/bounds.js'
+// import { Vec } from '~/math/vector.js'
+// import {
+//   updateSpriteSource,
+//   updateSpriteWidthHeight,
+// } from '~/spawnable/args.js'
+// import { createSpawnableEntity } from '~/spawnable/spawnableEntity.js'
+// import type { SpawnableEntity } from '~/spawnable/spawnableEntity.js'
+// import { createSprite, SpriteSourceSchema } from '~/textures/sprites.js'
+// import type { Debug } from '~/utils/debug.js'
+// import { drawBox } from '~/utils/draw.js'
+// import type { RedrawBox } from '~/utils/draw.js'
+
+// interface Data {
+//   debug: Debug
+// }
+
+// interface Render {
+//   camera: Camera
+//   container: Container
+//   gfx: Graphics
+//   redrawGfx: RedrawBox
+//   sprite: Sprite | undefined
+// }
+
+// export const createNonsolid = createSpawnableEntity<
+//   Args,
+//   SpawnableEntity<Data, Render, Args>,
+//   Data,
+//   Render
+// >(ArgsSchema, ({ transform }, args) => ({
+//   rectangleBounds() {
+//     return { width: args.width, height: args.height }
+//   },
+
+//   isPointInside(point) {
+//     return simpleBoundsTest(
+//       { width: args.width, height: args.height },
+//       transform,
+//       point,
+//     )
+//   },
+
+//   init({ game }) {
+//     return { debug: game.debug }
+//   },
+
+//   initRenderContext(_, { stage, camera }) {
+//     const { width, height, spriteSource } = args
+
+//     const container = new Container()
+//     container.sortableChildren = true
+//     container.zIndex = transform.zIndex
+
+//     const gfx = new Graphics()
+//     gfx.zIndex = 100
+//     const redrawGfx = drawBox(gfx, { width, height }, { stroke: 'blue' })
+
+//     const sprite = spriteSource
+//       ? createSprite(spriteSource, { width, height })
+//       : undefined
+
+//     container.addChild(gfx)
+//     if (sprite) container.addChild(sprite)
+//     stage.addChild(container)
+
+//     transform.addZIndexListener(() => {
+//       container.zIndex = transform.zIndex
+//     })
+
+//     return { camera, container, gfx, redrawGfx, sprite }
+//   },
+
+//   onArgsUpdate(path, _previous, _data, render) {
+//     updateSpriteWidthHeight(path, render?.sprite, args)
+
+//     if (render && (path === 'width' || path === 'height')) {
+//       render.redrawGfx(args)
+//     }
+
+//     updateSpriteSource(
+//       path,
+//       'sprite',
+//       'spriteSource',
+//       'container',
+//       args,
+//       render,
+//     )
+//   },
+
+//   onResize({ width, height }) {
+//     args.width = width
+//     args.height = height
+//   },
+
+//   teardown(_) {
+//     // No-op
+//   },
+
+//   teardownRenderContext({ gfx, sprite }) {
+//     gfx.destroy()
+//     sprite?.destroy()
+//   },
+
+//   onRenderFrame(_, { debug }, { camera, container, gfx }) {
+//     const pos = Vec.add(transform.position, camera.offset)
+
+//     container.position = pos
+//     container.angle = transform.rotation
+//     gfx.alpha = debug.value ? 0.5 : 0
+//   },
+// }))
--- src/entities/spawnable/solid.ts
+++ src/entities/spawnable/solid.ts
@@ -1,147 +1,49 @@
 import Matter from 'matter-js'
-import { Container, Graphics } from 'pixi.js'
-import type { Sprite } from 'pixi.js'
-import { z } from 'zod'
-import type { Camera } from '~/entities/camera.js'
-import { toRadians } from '~/math/general.js'
-import { Vec } from '~/math/vector.js'
-import type { Physics } from '~/physics.js'
-import {
-  updateBodyWidthHeight,
-  updateSpriteSource,
-  updateSpriteWidthHeight,
-} from '~/spawnable/args.js'
-import { createSpawnableEntity } from '~/spawnable/spawnableEntity.js'
-import type { SpawnableEntity } from '~/spawnable/spawnableEntity.js'
-import { createSprite, SpriteSourceSchema } from '~/textures/sprites.js'
-import type { Debug } from '~/utils/debug.js'
-import { drawBox } from '~/utils/draw.js'
-import type { RedrawBox } from '~/utils/draw.js'
+import type { InitContext } from '~/entity'
+import { toRadians } from '~/math/general'
+import type { Vector } from '~/math/vector'
+import type { Physics } from '~/physics'
+import { NonSolid } from './nonsolid'
 
-type Args = typeof ArgsSchema
-const ArgsSchema = z.object({
-  width: z.number().positive().min(1).default(100),
-  height: z.number().positive().min(1).default(100),
-  spriteSource: SpriteSourceSchema.optional(),
-})
+export class Solid extends NonSolid {
+  protected override readonly stroke = 'red'
 
-interface Data {
-  debug: Debug
-  physics: Physics
-}
+  protected declare physics: Physics
+  protected declare body: Matter.Body
 
-interface Render {
-  camera: Camera
-  container: Container
-  gfx: Graphics
-  redrawGfx: RedrawBox
-  sprite: Sprite | undefined
-}
-
-export const createSolid = createSpawnableEntity<
-  Args,
-  SpawnableEntity<Data, Render, Args>,
-  Data,
-  Render
->(ArgsSchema, ({ transform, preview }, args) => {
-  const body = Matter.Bodies.rectangle(
-    transform.position.x,
-    transform.position.y,
-    args.width,
-    args.height,
-    {
-      label: 'solid',
-      render: { visible: false },
-      angle: toRadians(transform.rotation),
-
-      isStatic: true,
-      isSensor: preview,
-      friction: 0,
-    },
-  )
-
-  return {
-    rectangleBounds() {
-      return { width: args.width, height: args.height }
-    },
-
-    isPointInside(position) {
-      return Matter.Query.point([body], position).length > 0
-    },
-
-    init({ game, physics }) {
-      const debug = game.debug
-      physics.register(this, body)
-      physics.linkTransform(body, transform)
-
-      return { debug, physics }
-    },
-
-    initRenderContext(_, { stage, camera }) {
-      const { width, height, spriteSource } = args
-
-      const container = new Container()
-      container.sortableChildren = true
-      container.zIndex = transform.zIndex
-
-      const gfx = new Graphics()
-      gfx.zIndex = 100
-      const redrawGfx = drawBox(gfx, { width, height })
-
-      const sprite = spriteSource
-        ? createSprite(spriteSource, { width, height })
-        : undefined
-
-      container.addChild(gfx)
-      if (sprite) container.addChild(sprite)
-      stage.addChild(container)
-
-      transform.addZIndexListener(() => {
-        container.zIndex = transform.zIndex
-      })
-
-      return { camera, container, gfx, redrawGfx, sprite }
-    },
-
-    onArgsUpdate(path, previous, _data, render) {
-      updateBodyWidthHeight(path, body, args, previous)
-      updateSpriteWidthHeight(path, render?.sprite, args)
-
-      if (render && (path === 'width' || path === 'height')) {
-        render.redrawGfx(args)
-      }
-
-      updateSpriteSource(
-        path,
-        'sprite',
-        'spriteSource',
-        'container',
-        args,
-        render,
-      )
-    },
-
-    onResize({ width, height }) {
-      args.width = width
-      args.height = height
-    },
-
-    teardown({ physics }) {
-      physics.unregister(this, body)
-      physics.unlinkTransform(body, transform)
-    },
-
-    teardownRenderContext({ gfx, sprite }) {
-      gfx.destroy()
-      sprite?.destroy()
-    },
-
-    onRenderFrame(_, { debug }, { camera, container, gfx }) {
-      const pos = Vec.add(transform.position, camera.offset)
-
-      container.position = pos
-      container.rotation = body.angle
-      gfx.alpha = debug.value ? 0.5 : 0
-    },
+  public override isPointInside(point: Vector): boolean {
+    return Matter.Query.point([this.body], point).length > 0
   }
-})
+
+  public override init(ctx: InitContext): void {
+    super.init(ctx)
+    const { transform, args } = this
+
+    this.body = Matter.Bodies.rectangle(
+      transform.position.x,
+      transform.position.y,
+      args.width,
+      args.height,
+      {
+        label: 'solid',
+        render: { visible: false },
+        angle: toRadians(transform.rotation),
+
+        isStatic: true,
+        // isSensor: preview,
+        friction: 0,
+      },
+    )
+
+    this.physics = ctx.physics
+    this.physics.register(this, this.body)
+    this.physics.linkTransform(this.body, this.transform)
+  }
+
+  public override teardown(): void {
+    super.teardown()
+
+    this.physics.unregister(this, this.body)
+    this.physics.unlinkTransform(this.body, this.transform)
+  }
+}
