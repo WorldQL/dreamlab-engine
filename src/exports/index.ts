189f271d931d6a1de26bc9a8fb8b693ca262b610
Jack Baron <jackmbaron@gmail.com>
2024-02-13 01:34:23 +0000

    clenaup exports

--- src/exports/index.ts
+++ src/exports/index.ts
@@ -1,26 +1,6 @@
 export { createGame, type Game } from '~/game.js'
-export {
-  createEntity,
-  dataManager,
-  isEntity,
-  type Entity,
-  type InitContext,
-  type Partialize,
-  type RenderContext,
-} from '~/entity.js'
+export * from '~/entity.js'
 export * from '~/spawnable/args.js'
-export {
-  SpawnableDefinitionSchema,
-  type SpawnableDefinition,
-  type LooseSpawnableDefinition,
-} from '~/spawnable/definition.js'
-export {
-  createSpawnableEntity,
-  isSpawnableEntity,
-  type PartializeSpawnable,
-  type SpawnableEntity,
-  type UID,
-  type SpawnableFunction,
-  type BareSpawnableFunction,
-} from '~/spawnable/spawnableEntity.js'
+export * from '~/spawnable/definition.js'
+export * from '~/spawnable/spawnableEntity.js'
 export type { Physics } from '~/physics.js'
