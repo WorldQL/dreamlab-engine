20ecc24b62228e2fac7abc6ffc319157e434c30c
Jack Baron <jackmbaron@gmail.com>
2024-02-14 13:17:45 +0000

    update defer methods to not need game

--- src/utils/defer.ts
+++ src/utils/defer.ts
@@ -1,58 +1,51 @@
-import { isPlayer } from '~/entities/player'
 import type { Player } from '~/entities/player'
-import type { RenderTime, Time } from '~/entity.js'
-import type { EventHandler } from '~/events.js'
-import type { Game } from '~/game.js'
+import { isPlayer } from '~/entities/player'
+import type { RenderTime, Time } from '~/entity'
+import type { EventHandler } from '~/events'
+import { events, game } from '~/labs/magic'
 
-export const waitForPhysicsStep = async (
-  game: Game<boolean>,
-): Promise<void> => {
+export const waitForPhysicsStep = async (): Promise<void> => {
   return new Promise<void>(resolve => {
-    game.events.common.once('onPhysicsStep', () => resolve())
+    events().common.once('onPhysicsStep', () => resolve())
   })
 }
 
-export const deferUntilPhysicsStep = (
-  game: Game<boolean>,
-  fn: (time: Time) => void,
-) => {
-  game.events.common.once('onPhysicsStep', fn)
+export const deferUntilPhysicsStep = (fn: (time: Time) => void) => {
+  events().common.once('onPhysicsStep', fn)
 }
 
-export const waitForRenderFrame = async (game: Game<false>): Promise<void> => {
+export const waitForRenderFrame = async (): Promise<void> => {
+  const $game = game('client', true)
   return new Promise<void>(resolve => {
-    game.events.client.once('onRenderFrame', () => resolve())
+    $game.events.client.once('onRenderFrame', () => resolve())
   })
 }
 
-export const deferUntilRenderFrame = (
-  game: Game<false>,
-  fn: (time: RenderTime) => void,
-) => {
-  game.events.client.once('onRenderFrame', fn)
+export const deferUntilRenderFrame = (fn: (time: RenderTime) => void) => {
+  const $game = game('client', true)
+  $game.events.client.once('onRenderFrame', fn)
 }
 
-export const waitForPlayer = async (game: Game<boolean>): Promise<Player> => {
-  const player = game.entities.find(isPlayer)
+export const waitForPlayer = async (): Promise<Player> => {
+  const $game = game()
+  const player = $game.entities.find(isPlayer)
   if (player) return player
 
   return new Promise<Player>(resolve => {
     const onInstantiate: EventHandler<'onInstantiate'> = entity => {
       if (isPlayer(entity)) {
-        game.events.common.removeListener('onInstantiate', onInstantiate)
+        $game.events.common.removeListener('onInstantiate', onInstantiate)
         resolve(entity)
       }
     }
 
-    game.events.common.addListener('onInstantiate', onInstantiate)
+    $game.events.common.addListener('onInstantiate', onInstantiate)
   })
 }
 
-export const deferUntilPlayer = (
-  game: Game<boolean>,
-  fn: (player: Player) => void,
-) => {
-  const player = game.entities.find(isPlayer)
+export const deferUntilPlayer = (fn: (player: Player) => void) => {
+  const $game = game('client', true)
+  const player = $game.entities.find(isPlayer)
   if (player) {
     fn(player)
     return
@@ -60,10 +53,10 @@ export const deferUntilPlayer = (
 
   const onInstantiate: EventHandler<'onInstantiate'> = entity => {
     if (isPlayer(entity)) {
-      game.events.common.removeListener('onInstantiate', onInstantiate)
+      $game.events.common.removeListener('onInstantiate', onInstantiate)
       fn(entity)
     }
   }
 
-  game.events.common.addListener('onInstantiate', onInstantiate)
+  $game.events.common.addListener('onInstantiate', onInstantiate)
 }
