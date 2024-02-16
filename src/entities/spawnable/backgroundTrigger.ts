2c198c1cb584c125fad76af2ba9114a6b67a1fb0
Jack Baron <jackmbaron@gmail.com>
2024-02-16 16:57:31 +0000

    port background and backgroundTrigger

--- src/entities/spawnable/backgroundTrigger.ts
+++ src/entities/spawnable/backgroundTrigger.ts
@@ -1,26 +1,37 @@
 import Matter from 'matter-js'
-import { Container, Graphics } from 'pixi.js'
+import onChange from 'on-change'
 import type { Sprite } from 'pixi.js'
+import { Container } from 'pixi.js'
 import { z } from 'zod'
-import type { Camera } from '~/entities/camera.js'
-import type { Background } from '~/entities/spawnable/background.js'
-import { isBackground } from '~/entities/spawnable/background.js'
-import type { EventHandler } from '~/events.js'
-import type { Game } from '~/game.js'
-import { createSpawnableEntity, dataManager } from '~/labs/compat'
-import type { LegacySpawnableEntity as SpawnableEntity } from '~/labs/compat'
-import { toRadians } from '~/math/general.js'
+import type { RenderTime } from '~/entity'
+import type { EventHandler } from '~/events'
+import {
+  camera,
+  debug,
+  events,
+  game,
+  isClient,
+  physics,
+  stage,
+} from '~/labs/magic'
+import type { Bounds } from '~/math/bounds'
+import { toRadians } from '~/math/general'
+import type { Vector } from '~/math/vector.js'
 import { Vec, VectorSchema } from '~/math/vector.js'
-import type { Physics } from '~/physics.js'
 import {
   updateBodyWidthHeight,
   updateSpriteWidthHeight,
-} from '~/spawnable/args.js'
+} from '~/spawnable/args'
+import type {
+  PreviousArgs,
+  SpawnableContext,
+} from '~/spawnable/spawnableEntity'
+import { SpawnableEntity } from '~/spawnable/spawnableEntity'
 import { createSprite } from '~/textures/sprites'
-import type { Debug } from '~/utils/debug.js'
-import { drawBox } from '~/utils/draw.js'
-import type { RedrawBox } from '~/utils/draw.js'
-import { onChange } from '~/utils/object'
+import { drawBox } from '~/utils/draw'
+import type { BoxGraphics } from '~/utils/draw'
+import { isBackground } from './background'
+import type { Background } from './background'
 
 enum BackgroundActionType {
   Clear = 'clear',
@@ -65,242 +76,209 @@ const ArgsSchema = z.object({
 })
 
 export { ArgsSchema as BackgroundTriggerArgs }
+export class BackgroundTrigger extends SpawnableEntity<Args> {
+  private readonly trigger: Matter.Body
+  private readonly container: Container | undefined
+  private readonly gfx: BoxGraphics | undefined
+  private sprite: Sprite | undefined
 
-interface Data {
-  game: Game<boolean>
-  debug: Debug
-  physics: Physics
+  private inside = false
 
-  onPlayerCollisionStart: EventHandler<'onPlayerCollisionStart'>
-  onPlayerCollisionEnd: EventHandler<'onPlayerCollisionEnd'>
-}
+  async #getBackground(): Promise<Background> {
+    const $game = game()
+    const existing = $game.queryType(isBackground)
+    if (existing) return existing
 
-interface Render {
-  camera: Camera
-  container: Container
-  gfx: Graphics
-  redrawGfx: RedrawBox
-  sprite: Sprite | undefined
-}
+    const spawned = $game.spawn({
+      entity: '@dreamlab/Background',
+      args: {},
+      transform: { position: [0, 0] },
+    })
 
-export const createBackgroundTrigger = createSpawnableEntity<
-  Args,
-  SpawnableEntity<Data, Render, Args>,
-  Data,
-  Render
->(ArgsSchema, ({ _this, transform }, args) => {
-  let inside = false
+    if (!spawned) {
+      throw new Error('failed to spawn background')
+    }
 
-  const colour = '#cc87ff'
-  const trigger = Matter.Bodies.rectangle(
-    transform.position.x,
-    transform.position.y,
-    args.width,
-    args.height,
-    {
-      label: 'background_trigger',
-      render: { visible: false },
-      angle: toRadians(transform.rotation),
+    if (!isBackground(spawned)) {
+      throw new Error('???')
+    }
 
-      isStatic: true,
-      isSensor: true,
-    },
-  )
+    return spawned
+  }
 
-  return {
-    rectangleBounds() {
-      return { width: args.width, height: args.height }
-    },
+  async #updateBackground(
+    action: z.infer<typeof BackgroundActionSchema>,
+  ): Promise<void> {
+    const background = await this.#getBackground()
 
-    isPointInside(position) {
-      return Matter.Query.point([trigger], position).length > 0
-    },
+    // This allows us to update the args without networking them
+    const args = onChange.target(background.args)
 
-    init({ game, physics }) {
-      const debug = game.debug
-      physics.register(_this, trigger)
-      physics.linkTransform(trigger, transform)
-
-      const getBackground = async (): Promise<Background> => {
-        const existing = game.queryType(isBackground)
-        if (existing) return existing
-
-        const spawned = await game.spawn({
-          entity: '@dreamlab/Background',
-          args: {},
-          transform: { position: [0, 0] },
-        })
-
-        if (!spawned) {
-          throw new Error('failed to spawn background')
+    switch (action.action) {
+      case BackgroundActionType.Set: {
+        if (action.fadeTime) {
+          args.fadeTime = action.fadeTime
+          if (isClient()) background.onArgsUpdate('fadeTime', background.args)
         }
 
-        return spawned as Background
-      }
-
-      const updateBackground = async (
-        action: z.infer<typeof BackgroundActionSchema>,
-      ) => {
-        const background = await getBackground()
-        // This allows us to update the args without networking them
-        const bgDirectRef = onChange.target<typeof background.definition>(
-          background.definition,
-        )
-        const data = dataManager.getData(background)
-        const render = dataManager.getRenderData(background)
-
-        switch (action.action) {
-          case BackgroundActionType.Set: {
-            if (action.fadeTime) bgDirectRef.args.fadeTime = action.fadeTime
-            if (action.scale) bgDirectRef.args.scale = action.scale
-            if (action.parallax) bgDirectRef.args.parallax = action.parallax
-
-            bgDirectRef.args.textureURL = action.textureURL
-            break
-          }
-
-          case BackgroundActionType.Clear: {
-            bgDirectRef.args.textureURL = undefined
-
-            break
-          }
-
-          case BackgroundActionType.Keep: {
-            // No-op
-            break
-          }
+        if (action.scale) {
+          args.scale = action.scale
+          if (isClient()) background.onArgsUpdate('scale', background.args)
         }
 
-        if (background.onArgsUpdate && render) {
-          background.onArgsUpdate(
-            'textureURL',
-            background.args,
-            data,
-            render ?? undefined,
-          )
-          background.onArgsUpdate(
-            'scale',
-            background.args,
-            data,
-            render ?? undefined,
-          )
+        if (action.parallax) {
+          args.parallax = action.parallax
+          if (isClient()) background.onArgsUpdate('parallax', background.args)
         }
+
+        args.textureURL = action.textureURL
+        if (isClient()) background.onArgsUpdate('textureURL', background.args)
+
+        break
       }
 
-      const onPlayerCollisionStart: EventHandler<
-        'onPlayerCollisionStart'
-      > = async ([_player, other]) => {
-        if (other !== trigger) return
-        inside = true
-        await updateBackground(args.onEnter)
+      case BackgroundActionType.Clear: {
+        args.textureURL = undefined
+        if (isClient()) background.onArgsUpdate('textureURL', background.args)
+
+        break
       }
 
-      const onPlayerCollisionEnd: EventHandler<
-        'onPlayerCollisionEnd'
-      > = async ([_player, other]) => {
-        if (other !== trigger) return
-        inside = false
-        await updateBackground(args.onLeave)
+      case BackgroundActionType.Keep: {
+        // No-op
+        break
       }
+    }
+  }
 
-      game.events.client?.addListener(
-        'onPlayerCollisionStart',
-        onPlayerCollisionStart,
-      )
+  readonly #onPlayerCollisionStart: EventHandler<'onPlayerCollisionStart'> =
+    async ([_, other]) => {
+      if (other !== this.trigger) return
+      this.inside = true
+      await this.#updateBackground(this.args.onEnter)
+    }
 
-      game.events.client?.addListener(
-        'onPlayerCollisionEnd',
-        onPlayerCollisionEnd,
-      )
+  readonly #onPlayerCollisionEnd: EventHandler<'onPlayerCollisionEnd'> =
+    async ([_, other]) => {
+      if (other !== this.trigger) return
+      this.inside = false
+      await this.#updateBackground(this.args.onLeave)
+    }
 
-      return {
-        game,
-        debug,
-        physics,
-        onPlayerCollisionStart,
-        onPlayerCollisionEnd,
-      }
-    },
+  public constructor(ctx: SpawnableContext<Args>) {
+    super(ctx)
 
-    initRenderContext(_, { stage, camera }) {
-      const { width, height } = args
+    this.trigger = Matter.Bodies.rectangle(
+      this.transform.position.x,
+      this.transform.position.y,
+      this.args.width,
+      this.args.height,
+      {
+        label: 'background_trigger',
+        render: { visible: false },
+        angle: toRadians(this.transform.rotation),
 
-      const container = new Container()
-      container.sortableChildren = true
-      container.zIndex = transform.zIndex
+        isStatic: true,
+        isSensor: true,
+      },
+    )
 
-      const gfx = new Graphics()
-      gfx.zIndex = 100
-      const redrawGfx = drawBox(gfx, { width, height }, { stroke: colour })
+    physics().register(this, this.trigger)
+    physics().linkTransform(this.trigger, this.transform)
 
-      const sprite =
-        args.onEnter.action === 'set'
-          ? createSprite({ url: args.onEnter.textureURL }, { width, height })
+    const $events = events('client')
+    $events?.on('onPlayerCollisionStart', this.#onPlayerCollisionStart)
+    $events?.on('onPlayerCollisionEnd', this.#onPlayerCollisionEnd)
+
+    const $game = game('client')
+    if ($game) {
+      const { width, height } = this.args
+
+      this.container = new Container()
+      this.container.sortableChildren = true
+      this.container.zIndex = this.transform.zIndex
+
+      this.gfx = drawBox({ width, height }, { stroke: '#cc87ff' })
+      this.gfx.zIndex = 100
+
+      this.sprite =
+        this.args.onEnter.action === 'set'
+          ? createSprite(
+              { url: this.args.onEnter.textureURL },
+              { width, height },
+            )
           : undefined
 
-      container.addChild(gfx)
-      if (sprite) container.addChild(sprite)
-      stage.addChild(container)
+      this.container.addChild(this.gfx)
+      if (this.sprite) this.container.addChild(this.sprite)
+      stage().addChild(this.container)
 
-      transform.addZIndexListener(() => {
-        container.zIndex = transform.zIndex
+      this.transform.addZIndexListener(() => {
+        if (this.container) this.container.zIndex = this.transform.zIndex
       })
-
-      return { camera, container, gfx, redrawGfx, sprite }
-    },
-
-    onArgsUpdate(path, previous, _data, render) {
-      updateBodyWidthHeight(path, trigger, args, previous)
-      updateSpriteWidthHeight(path, render?.sprite, args)
-
-      if (render && (path === 'width' || path === 'height')) {
-        render.redrawGfx(args)
-      }
-
-      if (render && path.startsWith('onEnter')) {
-        const { width, height } = args
-
-        render.sprite?.destroy()
-        render.sprite =
-          args.onEnter.action === 'set'
-            ? createSprite({ url: args.onEnter.textureURL }, { width, height })
-            : undefined
-
-        if (render.sprite) render.container.addChild(render.sprite)
-      }
-    },
-
-    onResize({ width, height }) {
-      args.width = width
-      args.height = height
-    },
-
-    teardown({ game, physics, onPlayerCollisionStart, onPlayerCollisionEnd }) {
-      physics.unregister(_this, trigger)
-      physics.unlinkTransform(trigger, transform)
-
-      game.events.client?.removeListener(
-        'onPlayerCollisionStart',
-        onPlayerCollisionStart,
-      )
-
-      game.events.client?.removeListener(
-        'onPlayerCollisionEnd',
-        onPlayerCollisionEnd,
-      )
-    },
-
-    teardownRenderContext({ container }) {
-      container.destroy({ children: true })
-    },
-
-    onRenderFrame(_, { debug }, { camera, container, sprite }) {
-      const pos = Vec.add(transform.position, camera.offset)
-
-      container.position = pos
-      container.angle = transform.rotation
-      container.alpha = debug.value ? 0.5 : 0
-      if (sprite) sprite.alpha = inside ? 0 : 1
-    },
+    }
   }
-})
+
+  public override teardown(): void {
+    const $events = events('client')
+    $events?.off('onPlayerCollisionStart', this.#onPlayerCollisionStart)
+    $events?.off('onPlayerCollisionEnd', this.#onPlayerCollisionEnd)
+
+    physics().unregister(this, this.trigger)
+    physics().unlinkTransform(this.trigger, this.transform)
+
+    this.container?.destroy({ children: true })
+  }
+
+  public override bounds(): Bounds | undefined {
+    return { width: this.args.width, height: this.args.height }
+  }
+
+  public override isPointInside(point: Vector): boolean {
+    return Matter.Query.point([this.trigger], point).length > 0
+  }
+
+  public override onArgsUpdate(
+    path: string,
+    previousArgs: PreviousArgs<Args>,
+  ): void {
+    updateBodyWidthHeight(path, this.trigger, this.args, previousArgs)
+    updateSpriteWidthHeight(path, this?.sprite, this.args)
+
+    if (this.gfx && (path === 'width' || path === 'height')) {
+      this.gfx.redraw(this.args)
+    }
+
+    if (this.container && path.startsWith('onEnter')) {
+      const { width, height } = this.args
+
+      this.sprite?.destroy()
+      this.sprite =
+        this.args.onEnter.action === 'set'
+          ? createSprite(
+              { url: this.args.onEnter.textureURL },
+              { width, height },
+            )
+          : undefined
+
+      if (this.sprite) this.container.addChild(this.sprite)
+    }
+  }
+
+  public override onResize(bounds: Bounds): void {
+    this.args.width = bounds.width
+    this.args.height = bounds.height
+  }
+
+  public override onRenderFrame(_: RenderTime): void {
+    const pos = Vec.add(this.transform.position, camera().offset)
+
+    if (this.container) {
+      this.container.position = pos
+      this.container.angle = this.transform.rotation
+      this.container.alpha = debug() ? 0.5 : 0
+    }
+
+    if (this.sprite) this.sprite.alpha = this.inside ? 0 : 1
+  }
+}
