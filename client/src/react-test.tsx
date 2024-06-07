144995d89d191f053eefab7f7c3a6f0a65ca3d78
Jackson Roberts <jacksonroberts25@gmail.com>
2024-06-07 12:40:26 -0600

    remove unused.

--- client/src/react-test.tsx
+++ client/src/react-test.tsx
@@ -1,12 +1,6 @@
 import { createRoot } from "react-dom/client";
 import EditorLayout from "./components/editor-layout.tsx";
 
-export const MyComponent = () => (
-  <div>
-    <p>hello!</p>
-  </div>
-);
-
 export const renderReact = () => {
   const root = createRoot(document.querySelector("#root")!);
   root.render(<EditorLayout />);
