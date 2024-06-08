ec99bf4f83b540d1c679e0c647002c57fa312577
Charlotte Som <charlotte@som.codes>
2024-06-08 08:56:04 +0000

    Sort mod.ts imports, move testcoords to _test
    Might want to move _main to _test as well (and give it a more
    descriptive name!)

--- engine/mod.ts
+++ engine/mod.ts
@@ -1,16 +1,9 @@
 export * from "./behavior/mod.ts";
-
 export * from "./entity/mod.ts";
-
 export * from "./game.ts";
-
-export * from "./physics.ts";
-
 export * from "./math/mod.ts";
-
+export * from "./physics.ts";
+export * from "./renderer/mod.ts";
 export * from "./signal.ts";
 export * from "./signals/mod.ts";
-
-export * from "./renderer/mod.ts";
-
 export * from "./value.ts";
