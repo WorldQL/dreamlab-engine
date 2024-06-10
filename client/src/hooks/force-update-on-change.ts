fc2fc2915c1e3b205d853c7f09add9c6fb293064
Christopher G <devcodedred@gmail.com>
2024-06-10 12:46:06 -0500

    add more listeners for hook

--- client/src/hooks/force-update-on-change.ts
+++ client/src/hooks/force-update-on-change.ts
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
 import { game } from "../game.ts";
-import { EntityDescendentDestroyed, EntityDescendentSpawned } from "@dreamlab/engine";
+import {
+  EntitySpawned,
+  EntityChildSpawned,
+  EntityDescendentSpawned,
+  EntityDestroyed,
+  EntityChildDestroyed,
+  EntityDescendentDestroyed,
+  EntityRenamed,
+} from "@dreamlab/engine";
 import { useForceUpdate } from "./force-update.ts";
 
 export const useForceUpdateOnEntityChange = () => {
@@ -8,14 +16,30 @@ export const useForceUpdateOnEntityChange = () => {
 
   useEffect(() => {
     const onEntitySpawned = () => forceUpdate();
+    const onEntityChildSpawned = () => forceUpdate();
+    const onEntityDescendentSpawned = () => forceUpdate();
     const onEntityDestroyed = () => forceUpdate();
+    const onEntityChildDestroyed = () => forceUpdate();
+    const onEntityDescendentDestroyed = () => forceUpdate();
+    const onEntityRenamed = () => forceUpdate();
 
-    game.world.on(EntityDescendentSpawned, onEntitySpawned);
-    game.world.on(EntityDescendentDestroyed, onEntityDestroyed);
+    game.world.on(EntitySpawned, onEntitySpawned);
+    game.world.on(EntityChildSpawned, onEntityChildSpawned);
+    game.world.on(EntityDescendentSpawned, onEntityDescendentSpawned);
+    game.world.on(EntityDestroyed, onEntityDestroyed);
+    game.world.on(EntityChildDestroyed, onEntityChildDestroyed);
+    game.world.on(EntityDescendentDestroyed, onEntityDescendentDestroyed);
+    game.world.on(EntityRenamed, onEntityRenamed);
 
     return () => {
-      //   game.world.removeListener(EntityDescendentSpawned, onEntitySpawned);
-      //   game.world.removeListener(EntityDescendentDestroyed, onEntityDestroyed);
+      // TODO: we don't need to do this right?
+      // game.world.removeListener(EntitySpawned, onEntitySpawned);
+      // game.world.removeListener(EntityChildSpawned, onEntityChildSpawned);
+      // game.world.removeListener(EntityDescendentSpawned, onEntityDescendentSpawned);
+      // game.world.removeListener(EntityDestroyed, onEntityDestroyed);
+      // game.world.removeListener(EntityChildDestroyed, onEntityChildDestroyed);
+      // game.world.removeListener(EntityDescendentDestroyed, onEntityDescendentDestroyed);
+      // game.world.removeListener(EntityRenamed, onEntityRenamed);
     };
   }, [forceUpdate]);
 };
