29951c1c637e3fe906a2ed33c3ac50cb4b6fc5b8
Jack Baron <jackmbaron@gmail.com>
2024-06-11 14:27:30 +0100

    more lerps

--- engine/math/lerp.ts
+++ engine/math/lerp.ts
@@ -1,7 +1,36 @@
 export const EPSILON = 0.00001;
 
+function clamp01(value: number): number {
+  if (value < 0) return 0;
+  if (value > 1) return 1;
+  return value;
+}
+
+/**
+ * Interpolates between `a` and `b` by `t`
+ *
+ * `t` is clamped between 0 and 1.
+ */
 export function lerp(a: number, b: number, t: number): number {
+  return a + (b - a) * clamp01(t);
+}
+
+/**
+ * Interpolates between `a` and `b` by `t` without clamping `t`
+ */
+export function lerpUnclamped(a: number, b: number, t: number): number {
   return a + (b - a) * t;
+}
+
+const TAU = Math.PI * 2;
+
+/**
+ * Same as {@link lerp} but makes sure the values interpolate correctly when they wrap around 360 degrees
+ */
+export function lerpAngle(a: number, b: number, t: number): number {
+  let delta = (b - a) % TAU;
+  if (delta > Math.PI) delta -= TAU;
+  return a + delta * clamp01(t);
 }
 
 export function smoothLerp(
