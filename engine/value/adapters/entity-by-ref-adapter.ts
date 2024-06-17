92e626ce3009924f3c8ca57678829ecaac34c61a
Charlotte Som <charlotte@som.codes>
2024-06-17 15:17:02 +0000

    values: Add EntityByRefAdapter

---
+++ engine/value/adapters/entity-by-ref-adapter.ts
@@ -0,0 +1,18 @@
+import { Entity } from "../../entity/mod.ts";
+import { JsonValue, ValueTypeAdapter } from "../data.ts";
+
+/**
+ * This supports a SyncedValue<Entity>
+ */
+export class EntityByRefAdapter extends ValueTypeAdapter<Entity | undefined> {
+  convertToPrimitive(value: Entity | undefined): JsonValue {
+    return value?.ref;
+  }
+  convertFromPrimitive(value: JsonValue): Entity | undefined {
+    if (value === undefined) return undefined;
+    if (typeof value !== "string")
+      throw new TypeError("An EntityByRef value should be a string!");
+    const ref: string = value;
+    return this.game.entities.lookupByRef(ref);
+  }
+}
--- engine/value/data.ts
+++ engine/value/data.ts
@@ -1,3 +1,5 @@
+import { Game } from "../game.ts";
+
 export type Primitive = string | number | boolean | undefined;
 
 type JsonArray = readonly JsonValue[];
@@ -5,8 +7,10 @@ type JsonObject = { [Key in string]?: JsonValue };
 export type JsonValue = Primitive | JsonArray | JsonObject;
 
 export abstract class ValueTypeAdapter<T> {
+  constructor(protected game: Game) {}
+
   abstract convertToPrimitive(value: T): JsonValue;
   abstract convertFromPrimitive(value: JsonValue): T;
 }
 
-export type AdapterTypeTag<T> = new () => ValueTypeAdapter<T>;
+export type AdapterTypeTag<T> = new (game: Game) => ValueTypeAdapter<T>;
