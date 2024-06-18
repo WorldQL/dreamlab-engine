c17dd1f65107c9bae4a3783b1f82745268e42334
Jack Baron <jackmbaron@gmail.com>
2024-06-18 14:11:59 +0100

    enable strict mode

--- client/src/editor-ui-main.tsx
+++ client/src/editor-ui-main.tsx
@@ -1,7 +1,12 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import EditorLayout from "./components/editor-layout.tsx";
 
 export const renderEditorUI = (gameDiv: HTMLDivElement) => {
   const root = createRoot(document.querySelector("#root")!);
-  root.render(<EditorLayout gameDiv={gameDiv} />);
+  root.render(
+    <StrictMode>
+      <EditorLayout gameDiv={gameDiv} />
+    </StrictMode>,
+  );
 };
