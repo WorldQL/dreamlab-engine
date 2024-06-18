479e59fb85727b2df06cc7f8568afb7d31e4516b
Jack Baron <jackmbaron@gmail.com>
2024-06-18 16:05:09 +0100

    demo wasd movement behavioir

---
+++ engine/behavior/behaviors/wasd-movement-behavior.ts
@@ -0,0 +1,23 @@
+import { Vector2 } from "../../math/mod.ts";
+import { Behavior } from "../behavior.ts";
+
+export default class WASDMovementBehavior extends Behavior {
+  speed = this.values.number("speed", 1);
+
+  #up = this.inputs.create("@wasd/up", "Move Up", "KeyW");
+  #down = this.inputs.create("@wasd/down", "Move Down", "KeyS");
+  #left = this.inputs.create("@wasd/left", "Move Left", "KeyA");
+  #right = this.inputs.create("@wasd/right", "Move Right", "KeyD");
+
+  onTick(): void {
+    const movement = new Vector2(0, 0);
+    if (this.#up.pressed) movement.y += 1;
+    if (this.#down.pressed) movement.y -= 1;
+    if (this.#right.pressed) movement.x += 1;
+    if (this.#left.pressed) movement.x -= 1;
+
+    this.entity.transform.position = this.entity.transform.position.add(
+      movement.normalize().mul((this.time.delta / 100) * this.speed.value),
+    );
+  }
+}
