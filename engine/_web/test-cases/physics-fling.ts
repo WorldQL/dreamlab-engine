11a2aee1f1bf63e10e28786cd8169eeef9152818
Jack Baron <jackmbaron@gmail.com>
2024-06-21 19:17:02 +0100

    destroy flung objects after 2 seconds

--- engine/_web/test-cases/physics-fling.ts
+++ engine/_web/test-cases/physics-fling.ts
@@ -1,13 +1,24 @@
-import { EntityUpdate, Rigidbody2D, Sprite2D } from "../../mod.ts";
+import { Behavior, EntityUpdate, Rigidbody2D, Sprite2D } from "../../mod.ts";
 import { PhysicsDebug, slider } from "../debug.ts";
 
 game.local.spawn({ type: PhysicsDebug, name: "PhysicsDebug" });
+
+class Lifetime extends Behavior {
+  #time = 0;
+  onTick(): void {
+    this.#time += this.time.delta;
+    if (this.#time >= 2000) {
+      this.entity.destroy();
+    }
+  }
+}
 
 setInterval(() => {
   const body = game.world.spawn({
     type: Rigidbody2D,
     name: "Rigidbody",
     transform: { position: { x: -8, y: 0 } },
+    behaviors: [{ type: Lifetime }],
     children: [
       {
         type: Sprite2D,
@@ -25,13 +36,12 @@ setInterval(() => {
 }, 500);
 
 slider(
-  { label: "smooth", min: 1, max: 20, value: 10 },
+  { label: "smooth", group: "camera", min: 1, max: 20, value: 10 },
   value => (camera.smooth.value = 1 / (value * value)),
 );
-slider({ label: "pos: x" }, value => (camera.transform.position.x = value));
-slider({ label: "pos: y" }, value => (camera.transform.position.y = value));
+slider({ label: "pos: x", min: -1 }, value => (camera.transform.position.x = value));
+slider({ label: "pos: y", min: -1 }, value => (camera.transform.position.y = value));
 slider({ label: "rot", max: Math.PI * 2 }, value => (camera.transform.rotation = value));
-
 slider(
   { label: "scale: x", value: 1, min: 1, max: 2 },
   value => (camera.transform.scale.x = value),
