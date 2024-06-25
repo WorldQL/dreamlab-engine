c21a405af7a9577fcc94994773529002d350a246
Jackson Roberts <jacksonroberts25@gmail.com>
2024-06-25 10:54:33 -0600

    working real tailwind!

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{html,js,tsx,ts}"],
+  darkMode: "class",
   plugins: [],
-}
-
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(var(--primary))",
+        primaryDark: "rgb(var(--primaryDark))",
+        primaryLight: "rgb(var(--primaryLight))",
+        secondary: "rgb(var(--secondary))",
+        secondaryDark: "rgb(var(--secondaryDark))",
+        secondaryLight: "rgb(var(--secondaryLight))",
+        green: "rgb(var(--green))",
+        greenDark: "rgb(var(--greenDark))",
+        greenLight: "rgb(var(--greenLight))",
+        yellow: "rgb(var(--yellow))",
+        yellowDark: "rgb(var(--yellowDark))",
+        yellowLight: "rgb(var(--yellowLight))",
+        red: "rgb(var(--red))",
+        redDark: "rgb(var(--redDark))",
+        redLight: "rgb(var(--redLight))",
+        background: "rgb(var(--background))",
+        card: "rgb(var(--card))",
+        textPrimary: "rgb(var(--textPrimary))",
+        textSecondary: "rgb(var(--textSecondary))",
+        gray: "rgb(var(--gray))",
+        icon: "rgb(var(--icon))",
+      },
+    },
+  },
+};
