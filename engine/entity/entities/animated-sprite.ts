5e12b8059279295242ddf4755eba1b5fc28712ab
Jack Baron <jackmbaron@gmail.com>
2024-07-08 16:39:00 +0100

    animated sprite alpha

--- engine/entity/entities/animated-sprite.ts
+++ engine/entity/entities/animated-sprite.ts
@@ -71,6 +71,7 @@ export class AnimatedSprite2D extends PixiEntity {
     this.#sprite.width = this.width * this.globalTransform.scale.x;
     this.#sprite.height = this.height * this.globalTransform.scale.y;
     this.#sprite.anchor.set(0.5);
+    this.#sprite.alpha = this.alpha;
     this.#sprite.animationSpeed = this.speed;
     this.#sprite.loop = this.loop;
     this.#sprite.play();
