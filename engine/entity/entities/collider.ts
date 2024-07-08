827b64bab149cc179029bde5f3833679232b3576
Jack Baron <jackmbaron@gmail.com>
2024-07-08 16:54:09 +0100

    collider entity

---
+++ engine/entity/entities/collider.ts
@@ -0,0 +1,52 @@
+import RAPIER from "@dreamlab/vendor/rapier.ts";
+import { Vector2 } from "../../math/mod.ts";
+import { EntityPreUpdate, EntityUpdate } from "../../signals/mod.ts";
+import { Entity, EntityContext } from "../entity.ts";
+
+export class RectCollider2D extends Entity {
+  public static readonly icon = "⬜";
+
+  collider: RAPIER.Collider;
+  #shape: RAPIER.Cuboid;
+
+  constructor(ctx: EntityContext) {
+    super(ctx);
+
+    this.collider = this.game.physics.world.createCollider(
+      RAPIER.ColliderDesc.cuboid(
+        this.globalTransform.scale.x / 2,
+        this.globalTransform.scale.y / 2,
+      ),
+    );
+    this.#shape = this.collider.shape as RAPIER.Cuboid;
+
+    // EntityPreUpdate happens before physics runs, so we can set the physics body to match our transform
+    this.on(EntityPreUpdate, () => {
+      if (!this.game.physics.enabled) return;
+
+      this.collider.setTranslation({
+        x: this.globalTransform.position.x,
+        y: this.globalTransform.position.y,
+      });
+      this.collider.setRotation(this.globalTransform.rotation);
+      this.#shape.halfExtents = {
+        x: this.globalTransform.scale.x / 2,
+        y: this.globalTransform.scale.y / 2,
+      };
+    });
+
+    // EntityUpdate happens after physics runs, so we can update our transform
+    // to reflect the movement of the physics body
+    this.on(EntityUpdate, () => {
+      if (!this.game.physics.enabled) return;
+
+      this.globalTransform.position = new Vector2(this.collider.translation());
+      this.globalTransform.rotation = this.collider.rotation();
+      this.globalTransform.scale = new Vector2(
+        this.#shape.halfExtents.x * 2,
+        this.#shape.halfExtents.y * 2,
+      );
+    });
+  }
+}
+Entity.registerType(RectCollider2D, "@core");
--- engine/entity/entities/mod.ts
+++ engine/entity/entities/mod.ts
@@ -1,6 +1,7 @@
 export * from "./basic-living.ts";
 export * from "./camera.ts";
 export * from "./clickable.ts";
+export * from "./collider.ts";
 export * from "./empty.ts";
 export * from "./gizmo.ts";
 export * from "./rigidbody.ts";
