a393ccab427243f674526dc1411c0ede87fef1f0
Jack Baron <jackmbaron@gmail.com>
2024-07-16 14:33:22 +0100

    add react query to editor

--- client/src/editor-ui-main.tsx
+++ client/src/editor-ui-main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // @deno-types="npm:@types/react@18.3.1"
 import { StrictMode } from "react";
 // @deno-types="npm:@types/react-dom@18.3/client"
@@ -5,13 +6,17 @@ import { createRoot } from "react-dom/client";
 import { EditorLayout } from "./components/editor-layout.tsx";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
 
+const queryClient = new QueryClient();
+
 export const renderEditorUI = (gameDiv: HTMLDivElement) => {
   const root = createRoot(document.querySelector("#root")!);
   root.render(
     <StrictMode>
-      <TooltipProvider delayDuration={200}>
-        <EditorLayout gameDiv={gameDiv} />
-      </TooltipProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider delayDuration={200}>
+          <EditorLayout gameDiv={gameDiv} />
+        </TooltipProvider>
+      </QueryClientProvider>
     </StrictMode>,
   );
 };
