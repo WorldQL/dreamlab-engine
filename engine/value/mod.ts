1075f2f49627625fad46b5ec0897c859fc937a49
Jack Baron <jackmbaron@gmail.com>
2024-07-19 08:05:07 +0100

    add missing exports

--- engine/value/mod.ts
+++ engine/value/mod.ts
@@ -3,4 +3,6 @@ export * from "./value.ts";
 export * from "./registry.ts";
 
 export * from "./adapters/entity-by-ref-adapter.ts";
+export * from "./adapters/enum-adapter.ts";
 export * from "./adapters/texture-adapter.ts";
+export * from "./adapters/vector-adapter.ts";
