d02d4bc828adf6bb833e5d21d68e3aadd3e710a6
Jack Baron <jackmbaron@gmail.com>
2024-07-22 17:55:43 +0100

    wip camera jitter investigation

---
+++ engine/_web/test-cases/fast.ts
@@ -0,0 +1,29 @@
+import {
+  GamePostRender,
+  GamePreRender,
+  GameRender,
+  GameTick,
+  Sprite2D,
+  Vector2,
+} from "../../mod.ts";
+
+export const sprite = game.local.spawn({
+  type: Sprite2D,
+  name: "sprite",
+  values: { texture: "https://lulu.dev/avatar.png" },
+});
+
+game.on(GameTick, () => {
+  sprite.pos.x += (game.time.delta / 1000) * 50;
+});
+
+game.on(GamePostRender, () => {
+  // console.log(sprite.interpolated.position.x);
+  camera.pos.assign(sprite.interpolated.position);
+  // camera.pos.assign(sprite.globalTransform.position);
+});
+
+camera.smooth = 1;
+camera.globalTransform.scale.assign(Vector2.splat(0.2));
+
+// camera.parent = sprite;
--- engine/entity/interpolated-entity.ts
+++ engine/entity/interpolated-entity.ts
@@ -3,8 +3,8 @@ import { EntityPreUpdate, EntityUpdate, GamePreRender } from "../signals/mod.ts"
 import { Entity, EntityContext } from "./entity.ts";
 
 export abstract class InterpolatedEntity extends Entity {
-  #lastRenderPos: IVector2 | undefined;
-  #lastRenderRot: number | undefined;
+  #prevPos: IVector2 = this.pos.bare();
+  #currentPos: IVector2 = this.pos.bare();
 
   protected readonly interpolated = {
     position: new Vector2(this.globalTransform.position),
@@ -14,37 +14,47 @@ export abstract class InterpolatedEntity extends Entity {
   constructor(ctx: EntityContext) {
     super(ctx);
 
-    let prevRenderPos: IVector2 | undefined;
-    let prevRenderRot: number | undefined;
+    // let prevRenderPos: IVector2 | undefined;
+    // let prevRenderRot: number | undefined;
 
-    this.on(EntityPreUpdate, () => {
-      prevRenderPos = this.globalTransform.position.bare();
-      prevRenderRot = this.globalTransform.rotation;
-    });
+    // this.on(EntityPreUpdate, () => {
+    //   prevRenderPos = this.globalTransform.position.bare();
+    //   prevRenderRot = this.globalTransform.rotation;
+    // });
 
     this.on(EntityUpdate, () => {
-      this.#lastRenderPos = prevRenderPos;
-      this.#lastRenderRot = prevRenderRot;
+      this.#prevPos = this.#currentPos;
+      this.#currentPos = this.pos.bare();
+      // this.#lastRenderPos = prevRenderPos;
+      // this.#lastRenderRot = prevRenderRot;
     });
 
     this.listen(this.game, GamePreRender, () => {
-      this.interpolated.position =
-        this.#lastRenderPos !== undefined
-          ? Vector2.lerp(
-              this.#lastRenderPos,
-              this.globalTransform.position,
-              this.game.time.partial,
-            )
-          : this.globalTransform.position;
-
-      this.interpolated.rotation =
-        this.#lastRenderRot !== undefined
-          ? lerpAngle(
-              this.#lastRenderRot,
-              this.globalTransform.rotation,
-              this.game.time.partial,
-            )
-          : this.globalTransform.rotation;
+      // this.interpolated.position = this.globalTransform.position;
+      // console.log({ prev: this.#prevPos.x, current: this.#currentPos.x });
+      // this.interpolated.position = this.pos;
+      // console.log(this.time.partial);
+      this.interpolated.position = Vector2.lerp(
+        this.#prevPos,
+        this.#currentPos,
+        this.game.time.partial,
+      );
+      // this.interpolated.position =
+      //   this.#lastRenderPos !== undefined
+      //     ? Vector2.lerp(
+      //         this.#lastRenderPos,
+      //         this.globalTransform.position,
+      //         this.game.time.partial,
+      //       )
+      //     : this.globalTransform.position;
+      // this.interpolated.rotation =
+      //   this.#lastRenderRot !== undefined
+      //     ? lerpAngle(
+      //         this.#lastRenderRot,
+      //         this.globalTransform.rotation,
+      //         this.game.time.partial,
+      //       )
+      //     : this.globalTransform.rotation;
     });
   }
 }
