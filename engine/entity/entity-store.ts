ed27ea3b6cd989c3cfdcae5fc6fcbe3fc3490f06
Jack Baron <jackmbaron@gmail.com>
2024-07-22 14:38:42 +0100

    speed up lookupByType

--- engine/entity/entity-store.ts
+++ engine/entity/entity-store.ts
@@ -3,6 +3,7 @@ import { Entity, EntityConstructor } from "./entity.ts";
 export class EntityStore {
   #entitiesById = new Map<string, Entity>();
   #entitiesByRef = new Map<string, Entity>();
+  #entitiesByType = new Map<EntityConstructor, Set<Entity>>();
 
   lookupById(id: string): Entity | undefined {
     return this.#entitiesById.get(id);
@@ -12,10 +13,16 @@ export class EntityStore {
     return this.#entitiesByRef.get(ref);
   }
 
-  lookupByType<T extends Entity>(type: EntityConstructor<T>): T[] {
-    return [...this.#entitiesById.values()].filter(
-      (entity): entity is T => entity instanceof type,
-    );
+  lookupByType<T extends Entity>(type: EntityConstructor<T, true>): readonly T[] {
+    const entities: T[] = [];
+    for (const [ctor, set] of this.#entitiesByType) {
+      // @ts-expect-error: i cant be bothered to typecast this
+      if (!(ctor === type || ctor.prototype instanceof type)) continue;
+      // @ts-expect-error: same as above
+      entities.push(...set.values());
+    }
+
+    return entities;
   }
 
   /** for internal use */
@@ -28,10 +35,20 @@ export class EntityStore {
 
     this.#entitiesByRef.set(entity.ref, entity);
     this.#entitiesById.set(entity.id, entity);
+
+    const type = entity.constructor as EntityConstructor;
+    const set = this.#entitiesByType.get(type) ?? new Set();
+    set.add(entity);
+
+    this.#entitiesByType.set(type, set);
   }
   /** for internal use */
   _unregister(entity: Entity) {
     this.#entitiesById.delete(entity.id);
     this.#entitiesByRef.delete(entity.ref);
+
+    const type = entity.constructor as EntityConstructor;
+    const set = this.#entitiesByType.get(type);
+    if (set) set.delete(entity);
   }
 }
