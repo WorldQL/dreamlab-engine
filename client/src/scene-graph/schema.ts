742fc29f5d095d6988adbdf9ff74859a875dc055
Jackson Roberts <jacksonroberts25@gmail.com>
2024-07-22 11:24:07 -0600

    update schema.

--- client/src/scene-graph/schema.ts
+++ client/src/scene-graph/schema.ts
@@ -11,15 +11,18 @@ export const EntityTypeSchema = z.string().describe("Entity Type");
 
 export const ResourceLocationSchema = z.string().describe("Resource URI");
 
-export const VectorSchema = z.tuple([z.number().describe("x"), z.number().describe("y")]);
+export const VectorSchema = z.object({
+  x: z.number(),
+  y: z.number(),
+});
 
 export const ValueSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
 
 export const SceneDescTransformSchema = z.object({
-  position: VectorSchema.default([0, 0]),
-  scale: VectorSchema.default([1, 1]),
+  position: VectorSchema.default({ x: 0, y: 0 }),
+  scale: VectorSchema.default({ x: 1, y: 1 }),
   rotation: z.number().default(0),
-  // TODO: zIndex
+  z: z.number().default(0),
 });
 
 export const SceneDescBehaviorSchema = z.object({
