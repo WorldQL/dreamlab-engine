a5408058f4be6dc08472d1eb1040734b66cf0146
Charlotte Som <wvlb@stems.zip>
2024-08-08 07:25:37 +0100

    how did that get there lol

--- editor/web/dist/vendor/type-fest.js
+++
@@ -1,7 +0,0 @@
-// deno polyfills for browser
-Symbol.dispose ??= Symbol.for("Symbol.dispose");
-Symbol.asyncDispose ??= Symbol.for("Symbol.asyncDispose");
-
-// ../engine/_deps/type-fest.ts
-export * from "type-fest";
-//# sourceMappingURL=type-fest.js.map
