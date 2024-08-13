90a92c2cbd433a03aa4b590ae451f9fbc90bc5eb
Jack Baron <jackmbaron@gmail.com>
2024-08-13 15:16:48 +0100

    render the name of the fake camera

--- engine/entity/entities/editor/editor-fake-camera.ts
+++ engine/entity/entities/editor/editor-fake-camera.ts
@@ -1,5 +1,6 @@
 import * as PIXI from "@dreamlab/vendor/pixi.ts";
 import { IVector2 } from "../../../math/vector/vector2.ts";
+import { EntityRenamed } from "../../../signals/mod.ts";
 import { Entity, EntityContext } from "../../entity.ts";
 import { PixiEntity } from "../../pixi-entity.ts";
 
@@ -15,10 +16,19 @@ export class EditorFakeCamera extends PixiEntity {
   smooth: number = 0.01;
 
   #gfx: PIXI.Graphics | undefined;
+  #text: PIXI.Text | undefined;
+
   #draw() {
     if (!this.#gfx) throw new Error("no graphics context");
-
+    if (!this.#text) throw new Error("no text object");
     const bounds = this.#bounds;
+
+    let text = this.name;
+    if (this.active) text += "(active)";
+    this.#text.text = text;
+    this.#text.x = bounds.x / -2;
+    this.#text.y = bounds.y / -2 - 0.33;
+
     this.#gfx
       .clear()
       .rect(bounds.x / -2, bounds.y / -2, bounds.x, bounds.y)
@@ -31,7 +41,7 @@ export class EditorFakeCamera extends PixiEntity {
     this.#bounds.x = bounds.x;
     this.#bounds.y = bounds.y;
 
-    if (this.#gfx) this.#draw();
+    if (this.#gfx && this.#text) this.#draw();
   }
   get bounds(): Readonly<IVector2> {
     return this.#bounds;
@@ -49,8 +59,21 @@ export class EditorFakeCamera extends PixiEntity {
     if (!this.container) return;
 
     this.#gfx = new PIXI.Graphics();
+    this.#text = new PIXI.Text({
+      style: {
+        fontFamily: "Iosevka",
+        fontSize: 120,
+        fill: "white",
+        align: "left",
+      },
+    });
+
+    this.#text.scale.set(0.002);
     this.#draw();
 
+    this.container.addChild(this.#text);
     this.container.addChild(this.#gfx);
+
+    this.on(EntityRenamed, () => this.#draw());
   }
 }
