3e5fedb21a6027dc200e1de745ed619a8f69e5b4
Jack Baron <jackmbaron@gmail.com>
2024-08-14 19:48:32 +0100

    uhh more debug refactor

--- engine/entity/entities/editor/_debug.ts
+++ engine/entity/entities/editor/_debug.ts
@@ -1,4 +1,8 @@
 import * as PIXI from "@dreamlab/vendor/pixi.ts";
+import { IVector2 } from "../../../math/mod.ts";
+import { EntityRenamed, EntityResize } from "../../../signals/mod.ts";
+import { Entity } from "../../entity.ts";
+import { PixiEntity } from "../../pixi-entity.ts";
 
 export type Label = { readonly container: PIXI.Container; readonly text: PIXI.Text };
 export const createLabel = (icon: string, text?: string): Label => {
@@ -24,3 +28,84 @@ export const createLabel = (icon: string, text?: string): Label => {
 
   return Object.freeze({ container, text: _text } satisfies Label);
 };
+
+abstract class DebugShape {
+  #entity: PixiEntity;
+
+  protected label: Label;
+  protected readonly gfx = new PIXI.Graphics();
+
+  protected readonly color: PIXI.ColorSource;
+  protected readonly alpha: number;
+  protected readonly width: number;
+
+  constructor({
+    entity,
+    suffix = "",
+    color = "white",
+    alpha = 0.8,
+    width = 0.02,
+  }: {
+    readonly entity: PixiEntity;
+    readonly suffix?: string;
+    readonly color?: PIXI.ColorSource;
+    readonly alpha?: number;
+    readonly width?: number;
+  }) {
+    this.#entity = entity;
+    // @ts-expect-error: private access
+    const container = this.#entity.container!;
+
+    this.#suffix = suffix;
+    const icon = (entity.constructor as typeof Entity).icon ?? "📦";
+    this.label = createLabel(icon, entity.name + this.#suffix);
+    container.addChild(this.label.container);
+    container.addChild(this.gfx);
+
+    this.color = color;
+    this.alpha = alpha;
+    this.width = width;
+
+    if (entity.bounds) this.redraw(entity.bounds);
+
+    this.#entity.on(EntityRenamed, () => {
+      this.label.text.text = this.#entity.name + this.#suffix;
+    });
+
+    this.#entity.on(EntityResize, () => {
+      if (this.#entity.bounds) this.redraw(this.#entity.bounds);
+    });
+  }
+
+  #suffix: string;
+  get suffix() {
+    return this.#suffix;
+  }
+  set suffix(value) {
+    this.#suffix = value;
+    this.label.text.text = this.#entity.name + this.#suffix;
+  }
+
+  abstract redraw(bounds: IVector2): void;
+}
+
+export class DebugSquare extends DebugShape {
+  redraw(bounds: IVector2): void {
+    const color = this.color;
+    const width = this.width;
+
+    this.label.container.x = bounds.x / -2 - 0.05;
+    this.label.container.y = bounds.y / -2 - 0.36;
+
+    this.gfx.alpha = this.alpha;
+    this.gfx
+      .clear()
+      .rect(bounds.x / -2, bounds.y / -2, bounds.x, bounds.y)
+      .stroke({ color, width, alignment: -1 })
+      .moveTo(bounds.x / -2 - width, bounds.y / -2 - width)
+      .lineTo(bounds.x / 2 + width, bounds.y / 2 + width)
+      .moveTo(bounds.x / -2 - width, bounds.y / 2 + width)
+      .lineTo(bounds.x / 2 + width, bounds.y / -2 - width)
+      .stroke({ color, width });
+  }
+}
--- engine/entity/entities/editor/editor-fake-camera.ts
+++ engine/entity/entities/editor/editor-fake-camera.ts
@@ -1,19 +1,16 @@
-import * as PIXI from "@dreamlab/vendor/pixi.ts";
-import { IVector2, Vector2 } from "../../../math/mod.ts";
-import { EntityRenamed, EntityResize } from "../../../signals/mod.ts";
+import { IVector2 } from "../../../math/mod.ts";
 import { ValueChanged } from "../../../value/mod.ts";
 import { Entity, EntityContext } from "../../entity.ts";
 import { PixiEntity } from "../../pixi-entity.ts";
 import { Camera } from "../camera.ts";
-import { createLabel, Label } from "./_debug.ts";
+import { DebugSquare } from "./_debug.ts";
 
 export class EditorFakeCamera extends PixiEntity {
   static {
     Entity.registerType(this, "@core");
   }
 
-  // intentionally different to distinguish from real camera
-  public static readonly icon = "📷";
+  public static readonly icon = "🎥";
   readonly bounds: Readonly<IVector2> = Object.freeze({
     x: Camera.TARGET_VIEWPORT_SIZE,
     y: Camera.TARGET_VIEWPORT_SIZE,
@@ -23,43 +20,7 @@ export class EditorFakeCamera extends PixiEntity {
   smooth: number = 0.01;
   unlocked: boolean = false;
 
-  #gfx: PIXI.Graphics | undefined;
-  #label: Label | undefined;
-
-  #draw() {
-    if (!this.#gfx) throw new Error("no graphics context");
-    if (!this.#label) throw new Error("no label object");
-
-    const bounds = Vector2.mul(this.bounds, this.globalTransform.scale);
-    this.#label.container.x = bounds.x / -2 - 0.05;
-    this.#label.container.y = bounds.y / -2 - 0.36;
-
-    const color: PIXI.ColorSource = 0xffffff;
-    const alpha = 0.8;
-    const width: number = 0.02;
-
-    this.#label.container.alpha = alpha;
-    this.#gfx.alpha = alpha;
-
-    this.#gfx
-      .clear()
-      .rect(bounds.x / -2, bounds.y / -2, bounds.x, bounds.y)
-      .stroke({ color, width, alignment: -1 })
-      .moveTo(bounds.x / -2 - width, bounds.y / -2 - width)
-      .lineTo(bounds.x / 2 + width, bounds.y / 2 + width)
-      .moveTo(bounds.x / -2 - width, bounds.y / 2 + width)
-      .lineTo(bounds.x / 2 + width, bounds.y / -2 - width)
-      .stroke({ color, width });
-  }
-
-  #updateText() {
-    if (!this.#label) throw new Error("no label object");
-
-    let text = this.name;
-    if (this.active) text += " (active)";
-    this.#label.text.text = text;
-  }
-
+  #debug: DebugSquare | undefined;
   constructor(ctx: EntityContext) {
     super(ctx);
 
@@ -72,21 +33,13 @@ export class EditorFakeCamera extends PixiEntity {
     super.onInitialize();
     if (!this.container) return;
 
-    this.#gfx = new PIXI.Graphics();
-    this.#label = createLabel(EditorFakeCamera.icon);
-
-    this.#draw();
-    this.#updateText();
-
-    this.container.addChild(this.#label.container);
-    this.container.addChild(this.#gfx);
-
-    this.on(EntityResize, () => this.#draw());
-    this.on(EntityRenamed, () => this.#updateText());
+    this.#debug = new DebugSquare({ entity: this, suffix: this.active ? " (active)" : "" });
 
     const activeValue = this.values.get("active");
     this.listen(this.game.values, ValueChanged, ({ value }) => {
-      if (value === activeValue) this.#updateText();
+      if (this.#debug && value === activeValue) {
+        this.#debug.suffix = this.active ? " (active)" : "";
+      }
     });
   }
 }
