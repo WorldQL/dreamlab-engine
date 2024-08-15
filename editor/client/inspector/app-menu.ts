f5a153b3744379db932a54d7ac6f31506a68c31f
Jack Baron <jackmbaron@gmail.com>
2024-08-15 19:40:57 +0100

    rough connected users count

--- editor/client/inspector/app-menu.ts
+++ editor/client/inspector/app-menu.ts
@@ -1,17 +1,31 @@
-import { ClientGame } from "@dreamlab/engine";
+import { ClientGame, PlayerJoined, PlayerLeft } from "@dreamlab/engine";
 import { InspectorUI, InspectorUIComponent } from "./inspector.ts";
 import { element as elem } from "@dreamlab/ui";
 
 export class AppMenu implements InspectorUIComponent {
   constructor(private game: ClientGame) {}
 
-  render(_ui: InspectorUI, editUIRoot: HTMLElement): void {
+  render(ui: InspectorUI, editUIRoot: HTMLElement): void {
     const topBar = editUIRoot.querySelector("#top-bar")!;
     const menu = elem("section", { id: "app-menu" }, [
       elem("div", {}, [elem("h1", {}, ["Dreamlab"]), elem("a", { role: "button" }, ["Save"])]),
       elem("div", {}, ["play / pause / stop"]),
-      elem("div", {}, ["stats"]),
+      elem("div", {}, [this.renderStats(ui, editUIRoot)]),
     ]);
     topBar.append(menu);
+  }
+
+  renderStats(_ui: InspectorUI, _editUIRoot: HTMLElement): HTMLElement {
+    const countText = document.createTextNode("1");
+    const updateCount = () => {
+      const count = this.game.network.connections.length;
+      countText.textContent = count.toLocaleString();
+    };
+
+    this.game.on(PlayerJoined, () => updateCount());
+    this.game.on(PlayerLeft, () => updateCount());
+    updateCount();
+
+    return elem("div", {}, ["Connected Users: ", countText]);
   }
 }
