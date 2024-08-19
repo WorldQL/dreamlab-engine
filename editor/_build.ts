9b8e228d930892bacc7842f65ef97d70936918b2
Jack Baron <jackmbaron@gmail.com>
2024-08-19 18:19:00 +0100

    optional clean for build

--- editor/_build.ts
+++ editor/_build.ts
@@ -1,13 +1,24 @@
 import {
-  bundleEngineDependencies,
-  bundleUI,
-  bundleEngine,
-  bundleStyles,
   bundleClient,
+  bundleEngine,
+  bundleEngineDependencies,
+  bundleStyles,
+  bundleUI,
 } from "../build-system/mod.ts";
 
 if (import.meta.main) {
   const watch = Deno.args.includes("--watch");
+  const clean = Deno.args.includes("--clean");
+
+  if (clean) {
+    try {
+      await Deno.remove("./web/dist", { recursive: true });
+    } catch (err) {
+      if (!(err instanceof Deno.errors.NotFound)) {
+        throw err;
+      }
+    }
+  }
 
   await bundleEngineDependencies("../engine/", "./web/dist");
   await bundleEngine("../engine/", "./web/dist", undefined, { watch });
