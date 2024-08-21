7e67d0f9003a40268bd143c4068c3e0ccd9025fe
Jack Baron <jackmbaron@gmail.com>
2024-08-21 18:58:01 +0100

    add simple icons

--- editor/client/_icons.ts
+++ editor/client/_icons.ts
@@ -1,7 +1,12 @@
+import type { SimpleIcon } from "npm:simple-icons";
+
 export * from "npm:lucide-static";
+export * from "npm:simple-icons";
 
 const parser = new DOMParser();
-export function icon(icon: string): Element {
-  const doc = parser.parseFromString(icon, "image/svg+xml");
+export function icon(icon: SimpleIcon | string): Element {
+  const svg = typeof icon === "string" ? icon : icon.svg;
+  const doc = parser.parseFromString(svg, "image/svg+xml");
+
   return doc.firstElementChild!;
 }
