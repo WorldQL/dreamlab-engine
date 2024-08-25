3a5ef2c7d13503e5c57da1b090563d8908eb37a9
Jackson Roberts <jacksonroberts25@gmail.com>
2024-08-25 03:11:44 -0600

    don't report client->server for Colliders named "Bullet"

--- client/src/networking/transform-sync.ts
+++ client/src/networking/transform-sync.ts
@@ -36,9 +36,19 @@ export const handleTransformSync: ClientNetworkSetupRoutine = (conn, game) => {
     }
 
     if (entityTransformReports.length > 0) {
+      // TODO: Remove this once DRE-890 is fixed
+      const _entityTransformReports = entityTransformReports.filter((entity => {
+        const e = game.entities.lookupByRef(entity.entity)
+        // insane hack to fix the space tutorial.
+        if (e?.name.startsWith("Bullet")) {
+          return false
+        }
+        return true
+      }))
+
       conn.send({
         t: "ReportEntityTransforms",
-        reports: entityTransformReports,
+        reports: _entityTransformReports,
       });
     }
 
