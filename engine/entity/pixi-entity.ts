5f1d86a1c99e476e9ce60e60ddc2b3ea6545cd70
Jack Baron <jackmbaron@gmail.com>
2024-09-02 17:38:31 +0100

    overhaul pixi entity it actually works now lol static or hidden disables render listener hidden or prefab tree disables rendering

--- engine/entity/pixi-entity.ts
+++ engine/entity/pixi-entity.ts
@@ -1,58 +1,14 @@
 import * as PIXI from "@dreamlab/vendor/pixi.ts";
 import { SignalSubscription } from "../signal.ts";
 import { EntityDestroyed, EntityReparented, GameRender } from "../signals/mod.ts";
+import { ValueChanged } from "../value/mod.ts";
 import { Entity, EntityConstructor, EntityContext } from "./entity.ts";
 
 export abstract class PixiEntity extends Entity {
   protected container: PIXI.Container | undefined;
 
-  constructor(ctx: EntityContext) {
-    super(ctx);
-
-    this.on(EntityReparented, () => {
-      if (!this.container) return;
-
-      // cull pixi container if in prefabs tree
-      const visible = this.root !== this.game.prefabs;
-      this.container.visible = visible;
-    });
-
-    this.defineValues(this.constructor as EntityConstructor<PixiEntity>, "static");
-
-    // force add the render listener if not static lmao
-    if (!this.static) {
-      this.#gameRenderListener = this.game.on(GameRender, () => {
-        this.#updateContainerPosition();
-      });
-
-      this.externalListeners.push(this.#gameRenderListener);
-    }
-
-    this.on(EntityDestroyed, () => {
-      this.container?.destroy({ children: true });
-    });
-  }
-
-  // NB(Charlotte):
-  // the idea here is that we don't need to interpolate any pixi entity that's not going to move,
-  // so we can save on a whole frame event listener when static is set to true.
-  // with 10,000 static sprites on firefox 130b9 this takes me from 50fps to 140fps
-  #gameRenderListener: SignalSubscription<GameRender> | undefined;
-  get static(): boolean {
-    return this.#gameRenderListener !== undefined;
-  }
-  set static(value: boolean) {
-    this.#gameRenderListener?.unsubscribe();
-    this.#gameRenderListener = undefined;
-
-    if (!value) {
-      this.#gameRenderListener = this.game.on(GameRender, () => {
-        this.#updateContainerPosition();
-      });
-
-      this.externalListeners.push(this.#gameRenderListener);
-    }
-  }
+  static: boolean = false;
+  hidden: boolean = false;
 
   #updateContainerPosition() {
     if (!this.container) return;
@@ -64,6 +20,69 @@ export abstract class PixiEntity extends Entity {
     this.container.zIndex = this.z;
   }
 
+  // NB(Charlotte):
+  // the idea here is that we don't need to interpolate any pixi entity that's not going to move,
+  // so we can save on a whole frame event listener when static is set to true.
+  // with 10,000 static sprites on firefox 130b9 this takes me from 50fps to 140fps
+  #gameRenderListener: SignalSubscription<GameRender> | undefined;
+  #updateTransformListeners() {
+    this.#gameRenderListener?.unsubscribe();
+    this.#gameRenderListener = undefined;
+
+    const shouldListen = !this.static && !this.hidden;
+    if (shouldListen) {
+      this.#gameRenderListener = this.game.on(GameRender, () => {
+        this.#updateContainerPosition();
+      });
+
+      this.externalListeners.push(this.#gameRenderListener);
+    }
+  }
+
+  #updateVisibility() {
+    if (!this.container) return;
+
+    // cull pixi container if in prefabs tree
+    const culled = this.root === this.game.prefabs;
+    const visible = !(this.hidden || culled);
+    this.container.visible = visible;
+  }
+
+  constructor(ctx: EntityContext) {
+    super(ctx);
+
+    const staticValue = this.defineValue(
+      this.constructor as EntityConstructor<PixiEntity>,
+      "static",
+    );
+
+    const hiddenValue = this.defineValue(
+      this.constructor as EntityConstructor<PixiEntity>,
+      "hidden",
+    );
+
+    // we cant use getter/setter pairs because this is an abstract class
+    this.listen(this.game.values, ValueChanged, ({ value }) => {
+      if (value === staticValue) {
+        this.#updateTransformListeners();
+      } else if (value === hiddenValue) {
+        this.#updateVisibility();
+        this.#updateTransformListeners();
+      }
+    });
+
+    // force add the render listener if not static
+    this.#updateTransformListeners();
+
+    this.on(EntityReparented, () => {
+      this.#updateVisibility();
+    });
+
+    this.on(EntityDestroyed, () => {
+      this.container?.destroy({ children: true });
+    });
+  }
+
   onInitialize() {
     if (!this.game.isClient()) return;
 
@@ -71,5 +90,6 @@ export abstract class PixiEntity extends Entity {
     this.game.renderer.scene.addChild(this.container);
 
     this.#updateContainerPosition();
+    this.#updateVisibility();
   }
 }
