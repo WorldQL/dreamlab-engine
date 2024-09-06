67be00827721e19abe38c7707db1f0edaa16ccf5
Jack Baron <jackmbaron@gmail.com>
2024-09-06 13:22:23 +0100

    wip: add categories to spawn menu

--- editor/client/util/entity-types.ts
+++ editor/client/util/entity-types.ts
@@ -10,23 +10,47 @@ export function getEntityTypes(): [type: EntityConstructor, namespace: string][]
     const key = `${namespace}/${ctor.name}`;
     reverseRegistry.set(key, [ctor, namespace]);
   }
-  return [...reverseRegistry.values()];
+  return [...reverseRegistry.values()].toSorted((a, b) => a[0].name.localeCompare(b[0].name));
 }
+
+// TODO: flesh out categories
+const categories = new Map<string, string[]>([
+  [
+    "Sprites",
+    ["@core/AnimatedSprite", "@core/Sprite", "@core/TilingSprite", "@core/VectorSprite"],
+  ],
+  ["UI", ["@core/UILayer", "@core/UIPanel"]],
+]);
 
 export function createEntityMenu(
   label: string,
   action: (type: EntityConstructor) => void,
 ): ContextMenuItem {
-  const entityTypes = getEntityTypes();
+  const entityTypes = getEntityTypes().filter(([_, namespace]) => namespace !== "@editor");
 
-  // TODO: categorize ?
+  const items: ContextMenuItem[] = [];
+  for (const [category, names] of categories) {
+    const categoryItems: ContextMenuItem[] = names
+      .map(name => {
+        const idx = entityTypes.findIndex(
+          ([type, namespace]) => `${namespace}/${type.name}` === name,
+        );
 
-  return [
-    label,
-    entityTypes
-      .filter(([_, namespace]) => namespace !== "@editor")
-      .map(([type, _]) => {
-        return [type.name, () => action(type)] as ContextMenuItem;
-      }),
-  ];
+        if (idx === -1) return undefined;
+        const [entity] = entityTypes.splice(idx, 1);
+        return entity;
+      })
+      .filter(item => item !== undefined)
+      .map(([type, _]) => [type.name, () => action(type)] satisfies ContextMenuItem);
+
+    items.push([category, categoryItems]);
+  }
+
+  items.push(
+    ...entityTypes.map(
+      ([type, _]) => [type.name, () => action(type)] satisfies ContextMenuItem,
+    ),
+  );
+
+  return [label, items] satisfies ContextMenuItem;
 }
