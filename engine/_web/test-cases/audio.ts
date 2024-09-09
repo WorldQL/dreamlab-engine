43756e71b31dfb28e566ebda41dafaa3ca6eb6af
Jack Baron <jackmbaron@gmail.com>
2024-09-09 12:23:42 +0100

    spatial audio support

--- engine/_web/test-cases/audio.ts
+++ engine/_web/test-cases/audio.ts
@@ -1,35 +1,26 @@
-import {
-  AudioSource,
-  Behavior,
-  Click,
-  ClickableRect,
-  Entity,
-  EntityByRefAdapter,
-  Sprite,
-} from "@dreamlab/engine";
+import { AudioSource, Behavior, Sprite } from "@dreamlab/engine";
+import WASDMovementBehavior from "../../behavior/behaviors/wasd-movement-behavior.ts";
 
 class PlaySoundBehavior extends Behavior {
-  #clickable = this.entity.cast(ClickableRect);
-  source: Entity | undefined;
+  #source = this.entity.cast(AudioSource);
+  #play = this.inputs.create("play", "Play Sound", "Space");
 
-  onInitialize(): void {
-    this.defineValue(PlaySoundBehavior, "source", { type: EntityByRefAdapter });
-
-    this.listen(this.#clickable, Click, () => {
-      this.source?.cast(AudioSource).play();
-    });
+  onTick() {
+    if (this.#play.pressed) this.#source.play();
   }
 }
 
 export const source = game.world.spawn({
   type: AudioSource,
   name: AudioSource.name,
-  values: { clip: "http://localhost:3000/test.ogg" },
+  values: { clip: "http://localhost:3000/test.ogg", maxRange: 20, minRange: 0.1, falloff: 0.1 },
+  transform: { scale: { x: 0.2, y: 0.2 } },
+  behaviors: [{ type: PlaySoundBehavior }],
+  children: [{ type: Sprite, name: Sprite.name }],
 });
 
-export const button = game.world.spawn({
-  type: ClickableRect,
-  name: ClickableRect.name,
-  children: [{ type: Sprite, name: Sprite.name }],
-  behaviors: [{ type: PlaySoundBehavior, values: { source } }],
-});
+// move source
+source.addBehavior({ type: WASDMovementBehavior });
+
+// move camera
+// camera.addBehavior({ type: WASDMovementBehavior });
--- engine/entity/entities/audio-source.ts
+++ engine/entity/entities/audio-source.ts
@@ -1,6 +1,9 @@
 import { Howl } from "@dreamlab/vendor/howler.ts";
+import { Vector2 } from "../../math/mod.ts";
+import { GameRender } from "../../signals/mod.ts";
 import { AudioAdapter } from "../../value/adapters/audio-adapter.ts";
 import { Entity, EntityContext } from "../entity.ts";
+import { Camera } from "./camera.ts";
 
 export class AudioSource extends Entity {
   static {
@@ -11,9 +14,11 @@ export class AudioSource extends Entity {
   readonly bounds = undefined;
 
   clip: string = "";
-  global: boolean = true; // TODO: spatial audio
   volume: number = 1;
   loop: boolean = false;
+  minRange: number = 0.2;
+  maxRange: number = Number.POSITIVE_INFINITY;
+  falloff: number = 0.8;
 
   #howl: Howl | undefined;
 
@@ -21,7 +26,7 @@ export class AudioSource extends Entity {
     super(ctx);
 
     this.defineValue(AudioSource, "clip", { type: AudioAdapter });
-    this.defineValues(AudioSource, "global", "volume", "loop");
+    this.defineValues(AudioSource, "volume", "loop", "minRange", "maxRange", "falloff");
 
     const clipValue = this.values.get("clip");
     clipValue?.onChanged(() => {
@@ -37,10 +42,22 @@ export class AudioSource extends Entity {
     loopValue?.onChanged(() => {
       if (this.#howl) this.#howl.loop(this.loop);
     });
+
+    const minRangeValue = this.values.get("minRange");
+    minRangeValue?.onChanged(() => this.#updateHRTF());
+    const maxRangeValue = this.values.get("maxRange");
+    maxRangeValue?.onChanged(() => this.#updateHRTF());
+    const falloffValue = this.values.get("falloff");
+    falloffValue?.onChanged(() => this.#updateHRTF());
+
+    this.listen(this.game, GameRender, () => {
+      this.#updatePosition();
+    });
   }
 
   #loadClip() {
     this.#howl = undefined;
+    if (!this.game.isClient()) return;
     if (this.clip === "") return;
 
     this.#howl = new Howl({
@@ -48,6 +65,47 @@ export class AudioSource extends Entity {
       volume: this.volume,
       loop: this.loop,
     });
+
+    this.#updateHRTF();
+  }
+
+  #updateHRTF() {
+    if (!this.#howl) return;
+    if (Number.isFinite(this.maxRange)) {
+      this.#howl.pannerAttr({
+        coneInnerAngle: 360,
+        coneOuterAngle: 360,
+        coneOuterGain: 0,
+        maxDistance: this.maxRange,
+        panningModel: "HRTF",
+        refDistance: this.minRange,
+        rolloffFactor: this.falloff,
+        distanceModel: "inverse",
+      });
+    } else {
+      this.#howl.pannerAttr({
+        coneInnerAngle: 360,
+        coneOuterAngle: 360,
+        coneOuterGain: 0,
+        distanceModel: "inverse",
+        maxDistance: 10000,
+        panningModel: "HRTF",
+        refDistance: 1,
+        rolloffFactor: 1,
+      });
+    }
+  }
+
+  #updatePosition() {
+    if (!this.#howl) return;
+    const camera = Camera.getActive(this.game);
+    if (!camera || !Number.isFinite(this.maxRange)) {
+      this.#howl.pos(0, 0, 0);
+      return;
+    }
+
+    const { x, y } = Vector2.sub(this.pos, camera.interpolated.position);
+    this.#howl.pos(x, y, 0);
   }
 
   onInitialize(): void {
