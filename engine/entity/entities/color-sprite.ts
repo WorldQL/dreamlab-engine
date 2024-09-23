ba6a25fc16dcb6b9f7df727a145181aec51618ca
Jack Baron <jackmbaron@gmail.com>
2024-09-23 14:49:35 +0100

    solid color entity

---
+++ engine/entity/entities/color-sprite.ts
@@ -0,0 +1,64 @@
+import * as PIXI from "@dreamlab/vendor/pixi.ts";
+import { IVector2, Vector2 } from "../../math/mod.ts";
+import { EntityTransformUpdate } from "../../signals/mod.ts";
+import { ColorAdapter } from "../../value/adapters/color-adapter.ts";
+import { Entity, EntityContext } from "../entity.ts";
+import { PixiEntity } from "../pixi-entity.ts";
+
+export class SolidColor extends PixiEntity {
+  static {
+    Entity.registerType(this, "@core");
+  }
+
+  public static readonly icon = "🟪";
+  get bounds(): Readonly<IVector2> | undefined {
+    // TODO: Reuse the same vector
+    return new Vector2(this.width, this.height);
+  }
+
+  width: number = 1;
+  height: number = 1;
+  color: string = "white";
+
+  #gfx: PIXI.Graphics | undefined;
+
+  constructor(ctx: EntityContext) {
+    super(ctx);
+
+    this.defineValues(SolidColor, "width", "height");
+    this.defineValue(SolidColor, "color", { type: ColorAdapter });
+
+    const updateGfx = () => {
+      this.#draw();
+    };
+
+    this.on(EntityTransformUpdate, updateGfx);
+    const widthValue = this.values.get("width");
+    const heightValue = this.values.get("height");
+    widthValue?.onChanged(updateGfx);
+    heightValue?.onChanged(updateGfx);
+
+    const colorValue = this.values.get("color");
+    colorValue?.onChanged(updateGfx);
+  }
+
+  #draw(): void {
+    if (!this.#gfx) return;
+
+    const color = new PIXI.Color(this.color);
+    this.#gfx
+      .clear()
+      .rect(-this.width / 2, -this.height / 2, this.width, this.height)
+      .fill({ color: color, alpha: color.alpha });
+  }
+
+  onInitialize() {
+    super.onInitialize();
+    if (!this.container) return;
+
+    this.#gfx = new PIXI.Graphics();
+    this.#draw();
+
+    this.container.addChild(this.#gfx);
+  }
+}
---
+++ engine/value/adapters/color-adapter.ts
@@ -0,0 +1,17 @@
+import * as PIXI from "@dreamlab/vendor/pixi.ts";
+import { JsonValue, ValueTypeAdapter } from "../data.ts";
+
+/**
+ * `Value<ColorAdapter>` is the same as `Value<string>`
+ */
+export class ColorAdapter extends ValueTypeAdapter<string> {
+  isValue(_value: unknown): _value is string {
+    return true;
+  }
+  convertToPrimitive(value: string): JsonValue {
+    return new PIXI.Color(value).toArray();
+  }
+  convertFromPrimitive(value: JsonValue): string {
+    return new PIXI.Color(value).toHexa();
+  }
+}
