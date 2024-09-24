d827a4cc158154c4da748cac73945e164e730be4
Christopher G <devcodedred@gmail.com>
2024-09-24 15:21:48 -0500

    smooth drag-resize

--- editor/client/draggable-layout.ts
+++ editor/client/draggable-layout.ts
@@ -6,32 +6,49 @@ const dragB = layout.querySelector("#bottom-bar-drag")! as HTMLElement;
 let leftDragging = false;
 let rightDragging = false;
 let bottomDragging = false;
+let animationFrame: number | null = null;
 
 document.addEventListener("pointerup", () => {
   leftDragging = false;
   rightDragging = false;
   bottomDragging = false;
+  document.body.classList.remove("col-resize", "row-resize");
+  if (animationFrame) {
+    cancelAnimationFrame(animationFrame);
+    animationFrame = null;
+  }
 });
 
 document.addEventListener("pointermove", e => {
+  if (!leftDragging && !rightDragging && !bottomDragging) return;
+
   const layoutRect = layout.getBoundingClientRect();
 
-  if (leftDragging) {
-    const width =
-      (e.clientX - layoutRect.left - dragL.getBoundingClientRect().width) / layoutRect.width;
-    layout.style.setProperty("--left-sidebar-width", `${(width * 100).toFixed(2)}%`);
+  if (animationFrame) {
+    cancelAnimationFrame(animationFrame);
   }
-  if (rightDragging) {
-    const width =
-      (layoutRect.right - e.clientX - dragR.getBoundingClientRect().width) / layoutRect.width;
-    layout.style.setProperty("--right-sidebar-width", `${(width * 100).toFixed(2)}%`);
-  }
-  if (bottomDragging) {
-    const height =
-      (layoutRect.bottom - e.clientY - dragB.getBoundingClientRect().height) /
-      layoutRect.height;
-    layout.style.setProperty("--bottom-bar-height", `${(height * 100).toFixed(2)}%`);
-  }
+
+  animationFrame = requestAnimationFrame(() => {
+    if (leftDragging) {
+      document.body.classList.add("col-resize");
+      const width =
+        (e.clientX - layoutRect.left - dragL.getBoundingClientRect().width) / layoutRect.width;
+      layout.style.setProperty("--left-sidebar-width", `${(width * 100).toFixed(2)}%`);
+    }
+    if (rightDragging) {
+      document.body.classList.add("col-resize");
+      const width =
+        (layoutRect.right - e.clientX - dragR.getBoundingClientRect().width) / layoutRect.width;
+      layout.style.setProperty("--right-sidebar-width", `${(width * 100).toFixed(2)}%`);
+    }
+    if (bottomDragging) {
+      document.body.classList.add("row-resize");
+      const height =
+        (layoutRect.bottom - e.clientY - dragB.getBoundingClientRect().height) /
+        layoutRect.height;
+      layout.style.setProperty("--bottom-bar-height", `${(height * 100).toFixed(2)}%`);
+    }
+  });
 });
 
 dragL.addEventListener("pointerdown", e => {
