f05ff4a1c65432252d3cd0c87380bedd430d4487
Jack Baron <jackmbaron@gmail.com>
2024-11-18 17:51:34 +0000

    add wait functions closes DRE-1007

--- engine/time.ts
+++ engine/time.ts
@@ -10,6 +10,7 @@ export class Time {
     this.#game = game;
   }
 
+  // #region Time Access
   #accessMode: "tick" | "render" = "tick";
   [internal.timeSetMode](mode: "tick" | "render") {
     this.#accessMode = mode;
@@ -18,6 +19,7 @@ export class Time {
   #ticks = 0;
   [internal.timeTick]() {
     this.#ticks += 1;
+    this.#flushQueue();
   }
   get ticks(): number {
     return this.#ticks;
@@ -46,6 +48,39 @@ export class Time {
     if (this.#accessMode === "tick") return 0;
     return this.#partial;
   }
+  // #endregion
+
+  // #region Wait Functions
+  #waitQueue = new Map<number, Set<() => void>>();
+  #flushQueue() {
+    const set = this.#waitQueue.get(this.#ticks);
+    if (!set) return;
+
+    // remove from map as soon as possible
+    this.#waitQueue.delete(this.#ticks);
+    for (const resolve of set) resolve();
+  }
+
+  public waitForNextTick(): Promise<void> {
+    return this.waitForTicks(1);
+  }
+
+  public waitForTicks(ticks: number): Promise<void> {
+    const { promise, resolve } = Promise.withResolvers<void>();
+
+    const scheduled = this.#ticks + ticks;
+    const set = this.#waitQueue.get(scheduled) ?? new Set();
+    set.add(resolve);
+    this.#waitQueue.set(scheduled, set);
+
+    return promise;
+  }
+
+  public waitForSeconds(seconds: number): Promise<void> {
+    const ticks = Math.round(seconds * this.TPS);
+    return this.waitForTicks(ticks);
+  }
+  // #endregion
 
   public toJSON() {
     return { now: this.now, delta: this.delta, partial: this.partial };
