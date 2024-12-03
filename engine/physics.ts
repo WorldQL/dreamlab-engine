2f40cb896aa84ce7e7e9ddabfc21f14e6ab45f01
Jack Baron <jackmbaron@gmail.com>
2024-12-03 17:56:55 +0000

    util fn to re-emit kinematic collision events closes DRE-1084

--- engine/physics.ts
+++ engine/physics.ts
@@ -1,4 +1,8 @@
-import RAPIER, { Collider, RigidBody } from "@dreamlab/vendor/rapier.ts";
+import RAPIER, {
+  Collider,
+  KinematicCharacterController,
+  RigidBody,
+} from "@dreamlab/vendor/rapier.ts";
 import { Entity } from "./entity/mod.ts";
 import { Game } from "./game.ts";
 import { EntityCollision } from "./signals/entity-collision.ts";
@@ -66,6 +70,53 @@ export class PhysicsEngine {
       entity1.fire(EntityCollision, started, entity2);
       entity2.fire(EntityCollision, started, entity1);
     });
+  }
+
+  emitCharacterControllerCollisions(
+    collider: Collider,
+    controller: KinematicCharacterController,
+  ): void;
+  emitCharacterControllerCollisions(combined: {
+    collider: Collider;
+    controller: KinematicCharacterController;
+  }): void;
+  emitCharacterControllerCollisions(
+    combined: { collider: Collider; controller: KinematicCharacterController } | Collider,
+    controller?: KinematicCharacterController,
+  ): void {
+    const collider: Collider = "collider" in combined ? combined.collider : combined;
+    const _controller = "controller" in combined ? combined.controller : controller;
+    if (!_controller) throw new TypeError("missing controller param");
+
+    const body1 = collider as ColliderWithUserData;
+    for (let i = 0; i < _controller.numComputedCollisions(); i++) {
+      const collision = _controller.computedCollision(i);
+      const body2 = (collision?.collider ?? undefined) as ColliderWithUserData | undefined;
+      if (!body2) continue;
+
+      const udata1 = body1?.userData;
+      const udata2 = body2?.userData;
+
+      let entityRef1: string | undefined;
+      let entityRef2: string | undefined;
+      if (udata1 && typeof udata1 === "object" && "entityRef" in udata1) {
+        entityRef1 = udata1.entityRef as string;
+      }
+      if (udata2 && typeof udata2 === "object" && "entityRef" in udata2) {
+        entityRef2 = udata2.entityRef as string;
+      }
+
+      if (!entityRef1 || !entityRef2) return;
+      const entity1 = this.game.entities.lookupByRef(entityRef1);
+      const entity2 = this.game.entities.lookupByRef(entityRef2);
+      if (!entity1 || !entity2) return;
+
+      // TODO: store collisions and emit end??
+      const started = true;
+
+      entity1.fire(EntityCollision, started, entity2);
+      entity2.fire(EntityCollision, started, entity1);
+    }
   }
 
   shutdown() {
