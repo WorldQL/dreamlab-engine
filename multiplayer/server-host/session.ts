78853ff0665ea3bed45ab06ef9420aa4fcffd838
Christopher G <devcodedred@gmail.com>
2024-12-05 21:13:52 -0600

    re-enable autosave

--- multiplayer/server-host/session.ts
+++ multiplayer/server-host/session.ts
@@ -100,7 +100,7 @@ export class GameSession {
         }
       };
 
-      // this.#autoSaveInterval = setInterval(save, 5000);
+      this.#autoSaveInterval = setInterval(save, 30000);
     }
   }
 
