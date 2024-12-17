4c37ad68cb18130f0160eefb80ff2f816d45ef41
Jack Baron <jackmbaron@gmail.com>
2024-12-17 13:55:22 +0000

    ignore all variants of dist
    Co-authored-by: Charlotte Som <charlotte@som.codes>

--- multiplayer/server-host/web/routes/script-editing.ts
+++ multiplayer/server-host/web/routes/script-editing.ts
@@ -44,7 +44,7 @@ export const serveScriptEditingAPI = (router: Router) => {
       const files: string[] = [];
       const entries = fs.expandGlob("**/*", {
         root: worldFolder,
-        exclude: ["node_modules", ".git", "_dist", "_dist_play", "*-esbuild.js"],
+        exclude: ["node_modules", ".git", "_dist", "_dist_*", "*-esbuild.js"],
       });
 
       for await (const entry of entries) {
