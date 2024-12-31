44311d84c9738306323819a56c2002a7742cb90a
Jack Baron <jackmbaron@gmail.com>
2024-12-31 14:26:21 +0000

    update test world

--- multiplayer/worlds/dreamlab/test-world/src/test-relative-value.ts
+++ multiplayer/worlds/dreamlab/test-world/src/test-relative-value.ts
@@ -1,9 +1,11 @@
-import { Behavior, Entity, RelativeEntity } from "@dreamlab/engine";
+import { Behavior, Entity, RelativeEntity, EntityRef } from "@dreamlab/engine";
 
 export default class Test extends Behavior {
-  e: Entity | undefined = undefined;
+  byRel: Entity | undefined = undefined;
+  byRef: Entity | undefined = undefined;
 
   override setup(): void {
-    this.defineValue(Test, "e", { type: RelativeEntity });
+    this.defineValue(Test, "byRel", { type: RelativeEntity });
+    this.defineValue(Test, "byRef", { type: EntityRef });
   }
 }
