77f2eefe452eb6268d98f2d73b0d6a513a47141d
Jack Baron <jackmbaron@gmail.com>
2024-12-31 14:28:48 +0000

    internally accept null values for entity adapters closes DRE-1112

--- engine/value/adapters/entity-by-ref-adapter.ts
+++ engine/value/adapters/entity-by-ref-adapter.ts
@@ -7,13 +7,15 @@ import { JsonValue, ValueTypeAdapter } from "../data.ts";
 export class EntityByRefAdapter extends ValueTypeAdapter<Entity | undefined> {
   isValue(value: unknown): value is Entity | undefined {
     if (value === undefined) return true;
+    if (value === null) return true;
     return value instanceof Entity;
   }
   convertToPrimitive(value: Entity | undefined): JsonValue {
-    return value?.ref;
+    return value?.ref ?? undefined;
   }
   convertFromPrimitive(value: JsonValue): Entity | undefined {
     if (value === undefined) return undefined;
+    if (value === null) return undefined;
     if (typeof value !== "string")
       throw new TypeError("An EntityByRef value should be a string!");
     const ref: string = value;
