9595c2f2b4939a43d74b2324dea824f051e1c7e4
Christopher G <devcodedred@gmail.com>
2025-01-14 13:52:26 -0600

    block new entity context menu during playmode

--- editor/client/ui/scene-graph.ts
+++ editor/client/ui/scene-graph.ts
@@ -98,6 +98,7 @@ export class SceneGraph implements InspectorUIWidget {
       ]);
     });
     this.game.renderer.app.canvas.addEventListener("contextmenu", event => {
+      if (!this.game.isEditMode) return;
       event.preventDefault();
       event.stopPropagation();
       const posAtRightClick = this.game.inputs.cursor.world;
