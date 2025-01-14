f31815e171d6bcc7898ba65ac08d3e7b1b414fa9
Jackson Roberts <jacksonroberts25@gmail.com>
2025-01-14 13:59:41 -0700

    tweak char controller.

--- engine/entity/entities/character-controller.ts
+++ engine/entity/entities/character-controller.ts
@@ -42,8 +42,8 @@ export class CharacterController extends Collider {
     }
 
     if (hasCollider) {
-      this.#controller = this.game.physics.world.createCharacterController(0.01);
-      this.#controller.enableSnapToGround(0.1);
+      this.#controller = this.game.physics.world.createCharacterController(0.05);
+      // this.#controller.enableSnapToGround(0.1);
       // TODO: Make this and sliding configurable.
       // sliding is super buggy especially with the rect collider.
       this.#controller.enableAutostep(0.5, 1, false)
