93710583814b36c02869030ca1ab876f13815b78
Jackson Roberts <jacksonroberts25@gmail.com>
2025-01-22 12:54:43 -0700

    Revert "fix more bad imports"
    This reverts commit 78635fb5208e88f5e5f7caae70e0719d6c261f2c.

--- engine/behavior/behavior.ts
+++ engine/behavior/behavior.ts
@@ -1,7 +1,7 @@
 import { createId } from "@dreamlab/vendor/nanoid.ts";
 import type { ConditionalExcept, Except } from "@dreamlab/vendor/type-fest.ts";
 
-import { Collider } from "../entity/entities/collider.ts";
+import { Collider, EntityCollision } from "@dreamlab/engine";
 // deno-lint-ignore no-unused-vars
 import type { Clickable } from "../entity/entities/clickable.ts"; // this is used in jsdoc
 import { Entity } from "../entity/mod.ts";
@@ -21,12 +21,9 @@ import {
   BehaviorDescendantSpawned,
   BehaviorDestroyed,
   BehaviorSpawned,
-  EntityCollision,
-  EntityEnableChanged,
-  GamePostTick,
-  GamePreTick,
-  GameRender,
-} from "../signals/mod.ts";
+} from "../signals/behavior-lifecycle.ts";
+import { GamePostTick, GamePreTick, GameRender } from "../signals/game-events.ts";
+import { EntityEnableChanged } from "../signals/mod.ts";
 import {
   AdapterTypeTag,
   JsonValue,
