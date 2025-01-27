ba2323822945cc8febcd78da91d94f7ffe35aac0
Charlotte Som <charlotte@som.codes>
2025-01-27 13:46:40 +0000

    multiplayer: split deno jsons into .host and .runtime

--- multiplayer/build-worker/build-engine.ts
+++ multiplayer/build-worker/build-engine.ts
@@ -4,9 +4,18 @@ await bundleEngine("../engine", "./engine-out", "./deno.json", { silent: true },
 
 const denoJson = await Deno.readTextFile("./deno.json").then(t => JSON.parse(t));
 for (const key of Object.keys(denoJson.imports)) {
-  denoJson.imports[key] = "../" + denoJson.imports[key];
+  if (denoJson.imports[key].startsWith("."))
+    denoJson.imports[key] = "../" + denoJson.imports[key];
 }
 denoJson.imports["@dreamlab/engine"] = "./engine.js";
 denoJson.lock = false;
+delete denoJson.tasks;
+await Deno.writeTextFile("./engine-out/deno.host.json", JSON.stringify(denoJson));
+
+for (const key of Object.keys(denoJson.imports)) {
+  if (!key.startsWith("@dreamlab/")) {
+    delete denoJson.imports[key];
+  }
+}
 delete denoJson.tasks;
 await Deno.writeTextFile("./engine-out/deno.runtime.json", JSON.stringify(denoJson));
