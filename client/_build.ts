45a6e1b9b6a8abb4f405bb8594958ecb35d0ee41
Charlotte Som <charlotte@som.codes>
2025-01-29 11:36:14 +0000

    put singleplayer runtime in web/runtime instead of web/dist
    more descriptive + this is going to get copied into output bundles

--- client/_build.ts
+++ client/_build.ts
@@ -9,13 +9,15 @@ import {
 if (import.meta.main) {
   const args = cli.parseArgs(Deno.args, {
     boolean: ["watch", "clean"],
-    string: ["serve-port"],
-    default: { "serve-port": "5179" },
+    string: ["serve-port", "out-dir", "serve-dir"],
+    default: { "serve-port": "5179", "out-dir": "./web/dist", "serve-dir": "./web" },
   });
+
+  const out = args["out-dir"];
 
   if (args.clean) {
     try {
-      await Deno.remove("./web/dist", { recursive: true });
+      await Deno.remove(out, { recursive: true });
     } catch (err) {
       if (!(err instanceof Deno.errors.NotFound)) {
         throw err;
@@ -23,17 +25,15 @@ if (import.meta.main) {
     }
   }
 
-  await bundleEngineDependencies("../engine/", "./web/dist");
-  await bundleEngine("../engine/", "./web/dist", undefined, { watch: args.watch });
-  await bundleUI("../ui/", "./web/dist");
-  await bundleClient(
-    ".",
-    "./web/dist",
-    "./deno.json",
-    [{ in: "./src/main.ts", out: "client-main" }],
-    {
-      watch: args.watch,
-      serve: { host: "127.0.0.1", port: Number(args["serve-port"]), servedir: "./web" },
+  await bundleEngineDependencies("../engine/", out);
+  await bundleEngine("../engine/", out, undefined, { watch: args.watch });
+  await bundleUI("../ui/", out);
+  await bundleClient(".", out, "./deno.json", [{ in: "./src/main.ts", out: "client-main" }], {
+    watch: args.watch,
+    serve: {
+      host: "127.0.0.1",
+      port: Number(args["serve-port"]),
+      servedir: args["serve-dir"],
     },
-  );
+  });
 }
--- singleplayer/_build-world.ts
+++ singleplayer/_build-world.ts
@@ -3,9 +3,6 @@ import { dirname, relative } from "jsr:@std/path@1";
 import { bundleWorld } from "../build-system/mod.ts";
 
 export async function bundleSingleplayerWorld(worldId: string) {
-  // 1. copy recursively from multiplayer/worlds
-  // 2.
-
   const sourceWorldDir = "../multiplayer/worlds/" + worldId;
   const targetWorldDir = "./web/worlds/" + worldId;
   const rel = relative(sourceWorldDir, targetWorldDir);
