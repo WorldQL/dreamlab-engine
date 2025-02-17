0ea21131b9bac707b29eeebac99df6b01d1939b7
Jack Baron <jackmbaron@gmail.com>
2025-02-17 16:54:49 +0000

    update zod

--- engine/_deps/zod.ts
+++ engine/_deps/zod.ts
@@ -1,8 +1,8 @@
-import type { UnknownKeysParam, ZodObject } from "https://deno.land/x/zod@v3.23.8/mod.ts";
+import type { UnknownKeysParam, ZodObject } from "https://deno.land/x/zod@v3.24.2/mod.ts";
 
 // deno-lint-ignore no-explicit-any
 export type ZodObjectAny = ZodObject<any, UnknownKeysParam>;
 
-export * from "https://deno.land/x/zod@v3.23.8/mod.ts";
+export * from "https://deno.land/x/zod@v3.24.2/mod.ts";
 
 export * as validationError from "npm:zod-validation-error@3.3.1";
