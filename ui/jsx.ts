c71ada112e4b06bb506c802b024b2e9f431ebcf4
Jackson Roberts <jacksonroberts25@gmail.com>
2025-02-18 23:37:48 -0700

    allow jsx to convert pascalCase props to kebab-case CSS. Fixes rules like fontSize not working.

--- ui/jsx.ts
+++ ui/jsx.ts
@@ -17,6 +17,20 @@ function Fragment(_props: Record<string, unknown>, _key?: string): never {
   throw new Error("fragments aren't supported yet :(");
 }
 
+// deno-lint-ignore no-explicit-any
+function convertKeysToKebabCase<T extends Record<string, any>>(obj: T): Record<string, any> {
+  // deno-lint-ignore no-explicit-any
+  const result: Record<string, any> = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      // Convert camelCase to kebab-case using regex
+      const kebabKey = key.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+      result[kebabKey] = obj[key];
+    }
+  }
+  return result;
+}
+
 function jsx<T extends keyof HTMLElementTagNameMap>(
   tag: T,
   props: Record<string, unknown>,
@@ -24,7 +38,11 @@ function jsx<T extends keyof HTMLElementTagNameMap>(
 ): HTMLElementTagNameMap[T] {
   const { children = [], classList, style, _also, ...attrs } = props;
   const childrenArray = Array.isArray(children) ? children : [children];
-  const extras = { classList, styleMap: style, _also } as ElementExtras<HTMLElementTagNameMap[T]>;
+  // deno-lint-ignore no-explicit-any
+  const convertedStyles = convertKeysToKebabCase(style as Record<string, any>);
+  const extras = { classList, styleMap: convertedStyles, _also } as ElementExtras<
+    HTMLElementTagNameMap[T]
+  >;
   return element(tag, attrs as ElementProps<HTMLElementTagNameMap[T]>, childrenArray, extras);
 }
 
