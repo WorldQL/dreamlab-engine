5257d7ca853e4f64de2d864fedf47164fe9e752b
Jack Baron <jackmbaron@gmail.com>
2025-03-10 18:26:40 +0000

    implement Random.unitVector2() correctly lol

--- engine/math/random.ts
+++ engine/math/random.ts
@@ -1,6 +1,23 @@
 import { Vector2 } from "./mod.ts";
 
 export class Random {
+  // #region utils
+  static #boxmuller(): readonly [number, number] {
+    let u = 0;
+    let v = 0;
+
+    while (u === 0) u = Math.random();
+    while (v === 0) v = Math.random();
+
+    const pi = 2 * Math.PI * v;
+    const R = Math.sqrt(-2 * Math.log(u));
+    const x = R * Math.cos(pi);
+    const y = R * Math.sin(pi);
+
+    return [x, y] as const;
+  }
+  // #endregion
+
   /**
    * Random value in the range [0,1) with uniform distribution
    *
@@ -13,33 +30,29 @@ export class Random {
   /**
    * Random value in the range (0,1) with normal (gaussian) distribution
    */
-  static gaussian(): number {
-    let u = 0;
-    let v = 0;
+  static normal(): number {
+    const [a, b] = this.#boxmuller();
+    const numA = a / 10.0 + 0.5; // translate to 0 -> 1
+    if (numA <= 1 && numA >= 0) return numA;
 
-    while (u === 0) u = Math.random();
-    while (v === 0) v = Math.random();
+    const numB = b / 10.0 + 0.5;
+    if (numB <= 1 && numB >= 0) return numB;
 
-    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-    num = num / 10.0 + 0.5; // Translate to 0 -> 1
-
-    if (num > 1 || num < 0) return Random.gaussian(); // resample between 0 and 1
-    return num;
+    return this.normal(); // resample between 0 and 1
   }
 
   /**
    * {@link Vector2} with x/y components sampled using [0,1) uniform distribution
    */
   static vector2(): Vector2 {
-    return new Vector2(Random.uniform(), Random.uniform());
+    return new Vector2(this.uniform(), this.uniform());
   }
 
   /**
    * {@link Vector2} randomly sampled from a unit circle
    */
   static unitVector2(): Vector2 {
-    const x = Random.gaussian();
-    const y = Random.gaussian();
+    const [x, y] = this.#boxmuller();
     return Vector2.normalize({ x, y });
   }
 }
