667f41efa6bba56e4bc3d9117c5351422fc7c4b2
Jackson Roberts <jacksonroberts25@gmail.com>
2025-03-19 17:30:34 -0600

    allow setCss on UIBehavior.

--- engine/behavior/ui-behavior.ts
+++ engine/behavior/ui-behavior.ts
@@ -3,6 +3,8 @@ import { Behavior, UILayer, UIPanel } from "@dreamlab/engine";
 export abstract class UIBehavior extends Behavior {
   private uiRoot: HTMLElement | undefined;
   public uiElement: HTMLElement | undefined;
+  private styleElement: HTMLStyleElement | undefined;
+  private cssContent: string = "";
 
   #ui: UILayer | UIPanel | undefined;
   #enablePointerEvents = true;
@@ -17,8 +19,48 @@ export abstract class UIBehavior extends Behavior {
     }
   }
 
+  /**
+   * Adds a CSS stylesheet that will be applied to the UI root element.
+   * This allows styling elements with class names used in your render method.
+   *
+   * @param cssString The CSS rules to apply
+   */
+  setCss(cssString: string): void {
+    if (!this.game.isClient()) return;
+
+    this.cssContent = cssString;
+
+    // If we already have a UI root and style element, update it
+    if (this.uiRoot) {
+      this.ensureStyleElement();
+    }
+  }
+
+  private ensureStyleElement(): void {
+    // Create style element if it doesn't exist
+    if (!this.styleElement) {
+      this.styleElement = document.createElement("style");
+      this.styleElement.id = `ui-behavior-style-${this.entity.id}`;
+
+      // Insert the style element at the beginning of the UI root
+      if (this.uiRoot?.firstChild) {
+        this.uiRoot.insertBefore(this.styleElement, this.uiRoot.firstChild);
+      } else if (this.uiRoot) {
+        this.uiRoot.appendChild(this.styleElement);
+      }
+    }
+
+    // Set the CSS content
+    if (this.styleElement) {
+      this.styleElement.textContent = this.cssContent;
+    }
+  }
+
   rerender() {
     if (this.uiRoot) {
+      // Ensure style element exists before rendering
+      this.ensureStyleElement();
+
       const newUI = this.render();
       if (this.uiElement && this.uiElement.parentNode === this.uiRoot) {
         this.uiRoot.replaceChild(newUI, this.uiElement);
@@ -42,6 +84,12 @@ export abstract class UIBehavior extends Behavior {
     }
 
     this.uiRoot = this.#ui.element;
+
+    // Now that we have a UI root, we can ensure the style element is created
+    if (this.cssContent) {
+      this.ensureStyleElement();
+    }
+
     this.rerender();
   }
 
@@ -57,6 +105,8 @@ export abstract class UIBehavior extends Behavior {
     if (this.uiRoot) {
       // uiElement still exists in memory after remove(), so we just re-append it.
       if (this.uiElement && this.uiElement.parentNode !== this.uiRoot) {
+        // Ensure the style element is first
+        this.ensureStyleElement();
         this.uiRoot.appendChild(this.uiElement);
       }
       if (this.uiElement) {
