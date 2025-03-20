8661048021dc6ba074e6b2c8b588e88959e81389
Charlotte Som <charlotte@som.codes>
2025-03-20 17:02:26 +0000

    allow loosely stringifiable types in css

--- ui/css.ts
+++ ui/css.ts
@@ -2,13 +2,15 @@ import type { WritableKeysOf } from "./_types.ts";
 
 type Style = CSSStyleDeclaration;
 
+type LooseStringifiable<T> = T extends string ? string | number | boolean : T;
+
 export type CSSProperties = {
   [K in WritableKeysOf<Style> as K extends number
     ? never
     : // deno-lint-ignore ban-types
       Style[K] extends Function
       ? never
-      : K]?: CSSStyleDeclaration[K];
+      : K]?: LooseStringifiable<CSSStyleDeclaration[K]>;
 };
 
 export type ExtendedCSSProperties = CSSProperties & { [custom: `--${string}`]: string };
