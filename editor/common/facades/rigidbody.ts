626958656c271893ffcca1c213c27cadeeecab6e
Charlotte Som <charlotte@som.codes>
2025-04-02 10:59:25 +0100

    run formatter

--- editor/common/facades/rigidbody.ts
+++ editor/common/facades/rigidbody.ts
@@ -1,10 +1,4 @@
-import {
-  Entity,
-  EntityContext,
-  enumAdapter,
-  PixiEntity,
-  Rigidbody,
-} from "@dreamlab/engine";
+import { Entity, EntityContext, enumAdapter, PixiEntity, Rigidbody } from "@dreamlab/engine";
 import { EnsureCompatible, EntityValueProps } from "./_compatibility.ts";
 import { Facades } from "./manager.ts";
 
