2490cccccfcf60eef8677549c6a22a33bf1505a4
Jack Baron <jackmbaron@gmail.com>
2025-04-09 15:44:56 +0100

    edit icon fn

--- editor/client/_icons.ts
+++ editor/client/_icons.ts
@@ -4,7 +4,9 @@ export * from "npm:lucide-static@0.429.0";
 export * from "npm:simple-icons@13.6.0";
 
 const parser = new DOMParser();
-export function icon(icon: SimpleIcon | string): Element {
+
+export type Icon = SimpleIcon | string;
+export function icon(icon: Icon): SVGElement {
   const svg = typeof icon === "string" ? icon : icon.svg;
   const doc = parser.parseFromString(svg, "image/svg+xml");
 
