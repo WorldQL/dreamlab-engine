323e9a139c633297acd0ca4af5f289d0463d840a
Jack Baron <jackmbaron@gmail.com>
2025-04-10 16:33:56 +0100

    add some todos

--- engine/synced-objects/mod.ts
+++ engine/synced-objects/mod.ts
@@ -1,7 +1,16 @@
-// TODO: selectively export
-
 export { sync } from "./decorator.ts";
 export type { AnySyncedObject, SyncedObject } from "./object.ts";
 export * from "./operation.ts";
 
 export * from "./objects/mod.ts";
+
+// TODO: hardcode mapping between field type and synced object type
+// use to expose `Behavior.sync.get("")` to give strongly typed SyncedObject
+// same for entities (low priority)
+// for listeners
+
+// TODO: deny update netcode and setting deep object
+// replicate by state
+// setup a get/set pair for the field itself to support `Behavior.myObject = {}`
+
+// TODO: Implement ArrayBuffer wrapper so we get TypedArray support for free
