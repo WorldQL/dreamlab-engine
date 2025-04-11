a32b017a267da47dc18a952c974ed3aca3387590
Charlotte Som <charlotte@som.codes>
2025-04-11 17:13:59 +0100

    don't log out the entity registry map

--- engine/entity/entity.ts
+++ engine/entity/entity.ts
@@ -1331,7 +1331,6 @@ export abstract class Entity implements ISignalHandler {
       !Entity.#entityTypeRegistry.has(target) &&
       !Reflect.get(target, internal.internalEntity)
     ) {
-      console.log(Entity.#entityTypeRegistry);
       throw new Error(`Entity type registry is missing ${target.name}!`);
     }
   };
