ef9f92b7819de613a94add8266f6ecd5fc01d702
Charlotte Som <charlotte@som.codes>
2025-04-11 12:09:10 +0100

    array: sync changes to length
    resize + set-at are all we need for everything provided there's never
    any clock mismatch, but this really should be a real sequence CRDT

--- engine/synced-objects/operation.ts
+++ engine/synced-objects/operation.ts
@@ -1,12 +1,15 @@
 import { z } from "@dreamlab/vendor/zod.ts";
-import { ArrayOperationPush, ArrayOperationSetAt } from "./objects/array.ts";
+import {
+  ArrayOperationPush,
+  ArrayOperationResize,
+  ArrayOperationSetAt,
+} from "./objects/array.ts";
 import { DeepObjectOperationSet } from "./objects/deep-object.ts";
-
-// TODO: grab the constituent ops from elsewhere (i.e. we put them in the file that defines the SyncedObject except for user defined ones)
 
 export const SyncedObjectOperationSchema = z.discriminatedUnion("t", [
   ArrayOperationPush,
   ArrayOperationSetAt,
+  ArrayOperationResize,
   DeepObjectOperationSet,
   z.object({
     t: z.literal("user-defined"),
