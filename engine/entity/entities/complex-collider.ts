e03dfd23ca738409c73900dbf91df4dd102ed935
Jack Baron <jackmbaron@gmail.com>
2025-04-24 15:56:44 +0100

    use quick decomp algo

--- engine/entity/entities/complex-collider.ts
+++ engine/entity/entities/complex-collider.ts
@@ -10,7 +10,7 @@ import {
   Rigidbody,
 } from "@dreamlab/engine";
 import * as internal from "@dreamlab/engine/internal";
-import { decomp, makeCCW } from "@dreamlab/vendor/poly-decomp-es.ts";
+import { quickDecomp as decomp, makeCCW } from "@dreamlab/vendor/poly-decomp-es.ts";
 import RAPIER, { RigidBody } from "@dreamlab/vendor/rapier.ts";
 
 export class ComplexCollider extends Entity {
