57e96162426ef192119b9f64685e50a6d39d85b7
Jackson Roberts <jacksonroberts25@gmail.com>
2025-05-03 15:10:54 -0600

    rm old sanity check log.

--- multiplayer/server-host/main.ts
+++ multiplayer/server-host/main.ts
@@ -7,8 +7,6 @@ import { createInstance, GameInstance } from "./instance.ts";
 import { report } from "./metrics.ts";
 import { setupWeb } from "./web/setup.ts";
 
-console.log("Booting dreamlab-engine/multiplayer, version after 2025-02-26");
-
 addEventListener("unhandledrejection", event => {
   event.preventDefault();
   if (event.reason) console.error("caught potential fatal error:", event.reason);
