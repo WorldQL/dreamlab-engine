efb1e45d20a42c3a22c90d9ded949aef55879286
Charlotte Som <charlotte@som.codes>
2025-05-12 16:39:29 +0100

    oops

--- multiplayer/play-host/actor-reporting.ts
+++ multiplayer/play-host/actor-reporting.ts
@@ -2,17 +2,28 @@ import { CONFIG } from "./config.ts";
 import { PlayInstance } from "./instance.ts";
 
 export const reportPlayerCount = async (instance: PlayInstance) => {
+  if (!CONFIG.SERVER_TRACKER || !CONFIG.MULTIPLAYER_AUTH_TOKEN || !CONFIG.ACTOR_ID) return;
+
   await fetch(`${CONFIG.SERVER_TRACKER}/rivet-server-admin/update-player-count`, {
     method: "POST",
     body: JSON.stringify({
       authToken: CONFIG.MULTIPLAYER_AUTH_TOKEN,
       playerCount: instance.connections.size,
-      actorId: CONFIG.RUNTIME_SCRIPT,
+      actorId: CONFIG.ACTOR_ID,
     }),
     headers: { "Content-Type": "application/json" },
   });
 };
 
 export const teardownActor = async (_instance: PlayInstance) => {
-  await fetch(`${CONFIG.SERVER_TRACKER}/`);
+  if (!CONFIG.SERVER_TRACKER || !CONFIG.MULTIPLAYER_AUTH_TOKEN || !CONFIG.ACTOR_ID) return;
+
+  await fetch(`${CONFIG.SERVER_TRACKER}/rivet-server-admin/delete-rivet-server`, {
+    method: "DELETE",
+    body: JSON.stringify({
+      authToken: CONFIG.MULTIPLAYER_AUTH_TOKEN,
+      actorId: CONFIG.ACTOR_ID,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
 };
