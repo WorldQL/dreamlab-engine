48652eff5d37c0a5029a2de05dfac0178ca8755a
Charlotte Som <charlotte@som.codes>
2025-05-12 16:48:54 +0100

    use url construction instead of concatenation here

--- multiplayer/play-host/actor-reporting.ts
+++ multiplayer/play-host/actor-reporting.ts
@@ -4,26 +4,32 @@ import { PlayInstance } from "./instance.ts";
 export const reportPlayerCount = async (instance: PlayInstance) => {
   if (!CONFIG.SERVER_TRACKER || !CONFIG.MULTIPLAYER_AUTH_TOKEN || !CONFIG.ACTOR_ID) return;
 
-  await fetch(`${CONFIG.SERVER_TRACKER}/api/project/rivet-server-admin/update-player-count`, {
-    method: "POST",
-    body: JSON.stringify({
-      authToken: CONFIG.MULTIPLAYER_AUTH_TOKEN,
-      playerCount: instance.connections.size,
-      actorId: CONFIG.ACTOR_ID,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+  await fetch(
+    new URL("/api/project/rivet-server-admin/update-player-count", CONFIG.SERVER_TRACKER),
+    {
+      method: "POST",
+      body: JSON.stringify({
+        authToken: CONFIG.MULTIPLAYER_AUTH_TOKEN,
+        playerCount: instance.connections.size,
+        actorId: CONFIG.ACTOR_ID,
+      }),
+      headers: { "Content-Type": "application/json" },
+    },
+  );
 };
 
 export const teardownActor = async (_instance: PlayInstance) => {
   if (!CONFIG.SERVER_TRACKER || !CONFIG.MULTIPLAYER_AUTH_TOKEN || !CONFIG.ACTOR_ID) return;
 
-  await fetch(`${CONFIG.SERVER_TRACKER}/api/project/rivet-server-admin/delete-rivet-server`, {
-    method: "DELETE",
-    body: JSON.stringify({
-      authToken: CONFIG.MULTIPLAYER_AUTH_TOKEN,
-      actorId: CONFIG.ACTOR_ID,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+  await fetch(
+    new URL("/api/project/rivet-server-admin/delete-rivet-server", CONFIG.SERVER_TRACKER),
+    {
+      method: "DELETE",
+      body: JSON.stringify({
+        authToken: CONFIG.MULTIPLAYER_AUTH_TOKEN,
+        actorId: CONFIG.ACTOR_ID,
+      }),
+      headers: { "Content-Type": "application/json" },
+    },
+  );
 };
