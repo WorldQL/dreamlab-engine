175bb08e1ec1a15e72b528e0ef76bb5bd73f6176
Charlotte Som <charlotte@som.codes>
2025-06-17 09:42:26 +0100

    we don't actually need to do anything about this, expect-error is fine

--- engine/entity/entity.ts
+++ engine/entity/entity.ts
@@ -587,7 +587,7 @@ export abstract class Entity implements ISignalHandler {
         z: this.transform.z,
       },
       values: entityValues,
-      // @ts-expect-error TODO: Fix this type mismatch
+      // @ts-expect-error hard-cast string -> Exclude<keyof this, …>
       sync: syncOverrides,
       data: this.saveDataForScene?.(),
     };
