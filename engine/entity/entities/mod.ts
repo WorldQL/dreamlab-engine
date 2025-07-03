7d7fd1e5f2b1e1a63baf68cf14bbdc1b6a6e10e5
Jackson Roberts <jacksonroberts25@gmail.com>
2025-07-03 15:33:53 -0600

    moved tilemap WIP to own branch.
    This reverts commit 0d03ad11dae5740969f79ebd652b3ce53e4a9040.

--- engine/entity/entities/mod.ts
+++ engine/entity/entities/mod.ts
@@ -12,7 +12,6 @@ export * from "./raw-pixi.ts";
 export * from "./rich-text.ts";
 export * from "./solid-color.ts";
 export * from "./sprite.ts";
-export * from "./tile-map.ts";
 export * from "./tiling-sprite.ts";
 export * from "./ui-layer.ts";
 export * from "./ui-panel.ts";
