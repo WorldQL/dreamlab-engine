0d03ad11dae5740969f79ebd652b3ce53e4a9040
Jack Baron <jackmbaron@gmail.com>
2025-07-03 21:40:08 +0100

    wip: tilemap

--- engine/entity/entities/mod.ts
+++ engine/entity/entities/mod.ts
@@ -12,6 +12,7 @@ export * from "./raw-pixi.ts";
 export * from "./rich-text.ts";
 export * from "./solid-color.ts";
 export * from "./sprite.ts";
+export * from "./tile-map.ts";
 export * from "./tiling-sprite.ts";
 export * from "./ui-layer.ts";
 export * from "./ui-panel.ts";
