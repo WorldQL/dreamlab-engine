5a95eae8f822cf1e1bef095e81dc2d47d24abfd7
Jack Baron <jackmbaron@gmail.com>
2025-07-07 16:37:05 +0100

    dont use dev auth in standalone

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -40,8 +40,7 @@ export const auth = async (nickname: string): Promise<AuthToken> => {
   const searchParams = new URLSearchParams(window.location.search);
   const passedToken = searchParams.get("token");
 
-  if ((globalThis.env.IS_DEV || globalThis.env.DREAMLAB_MULTIPLAYER_STANDALONE) && !passedToken)
-    return devAuth(nickname);
+  if (globalThis.env.IS_DEV && !passedToken) return devAuth(nickname);
 
   if (passedToken) return decodeToken(passedToken);
 
