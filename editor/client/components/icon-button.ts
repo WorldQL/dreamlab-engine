6f2688a6951fe8dde94b18fdab85fff0b360fafc
Christopher G <devcodedred@gmail.com>
2025-08-05 14:49:40 -0500

    add select none to icon button

--- editor/client/components/icon-button.ts
+++ editor/client/components/icon-button.ts
@@ -17,7 +17,11 @@ export class IconButton extends HTMLElement {
     const children: Array<string | Element | Text> = [icon(svg) as Element];
     if (label)
       children.push(
-        elem("span", { className: "label", style: { paddingRight: "0.4rem", fontSize: '14px' } }, [label]),
+        elem(
+          "span",
+          { className: "label", style: { paddingRight: "0.4rem", fontSize: "14px" } },
+          [label],
+        ),
       );
 
     const element = elem("button", { ...attrs, type: "button" }, children);
