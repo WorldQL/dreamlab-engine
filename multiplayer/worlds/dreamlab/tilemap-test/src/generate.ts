8d6aeae7d3459768e0e9f29ce9c87423edc4fbcb
Charlotte Som <charlotte@som.codes>
2025-08-15 15:17:59 +0100

    tilemap-test update

--- multiplayer/worlds/dreamlab/tilemap-test/src/generate.ts
+++ multiplayer/worlds/dreamlab/tilemap-test/src/generate.ts
@@ -9,22 +9,26 @@ export default class GenerateBehavior extends Behavior {
   onInitialize(): void {
     if (!this.game.isServer()) return;
 
+    setTimeout(() => {
       const then = performance.now();
 
       const ys = [];
       const xs = [];
       const tileIds = [];
 
+      const choices = [168, 544, 631, 719]
+
       for (let y = 0; y < this.width; y++) {
         for (let x = 0; x < this.width; x++) {
           ys.push(y);
           xs.push(x);
-          tileIds.push(Math.floor(Math.random() * 6));
+          tileIds.push(choices[Math.floor(Math.random() * choices.length)]);
         }
       }
 
       this.entity.cast(Tilemap).setTiles(xs, ys, tileIds);
 
-      console.log(`spawned tiles in ${(performance.now() - then).toFixed(3)}ms`)
+      console.log(`spawned tiles in ${(performance.now() - then).toFixed(3)}ms`);
+    }, 2500)
   }
 }
