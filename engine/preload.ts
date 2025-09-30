118a152bd696fdbdc72a15600c2681001181e703
Jack Baron <jackmbaron@gmail.com>
2025-09-30 03:19:28 +0100

    preload more things

--- engine/preload.ts
+++ engine/preload.ts
@@ -4,6 +4,9 @@ import * as PIXI from "@dreamlab/vendor/pixi.ts";
 
 export type PreloadInfo = {
   readonly textures?: string[];
+  readonly images?: string[];
+  readonly spritesheets?: string[];
+  readonly audioClips?: string[];
   // TODO: more things
 
   readonly custom?: () => void | Promise<void>;
@@ -22,6 +25,32 @@ export async function preload(game: ClientGame, info: PreloadInfo): Promise<void
   if (info.textures) {
     const textures = info.textures.map(url => game.resolveResource(url));
     jobs.push(PIXI.Assets.load(textures));
+  }
+
+  if (info.images) {
+    const images = info.images.map(url => game.resolveResource(url));
+    const job = images.map(async url => {
+      try {
+        const img = new Image();
+        img.src = url;
+        await img.decode();
+      } catch {
+        // ignore
+      }
+    });
+
+    jobs.push(Promise.all(job));
+  }
+
+  if (info.spritesheets) {
+    const spritesheets = info.spritesheets.map(url => game.resolveResource(url));
+    jobs.push(PIXI.Assets.load(spritesheets));
+  }
+
+  if (info.audioClips) {
+    const _audioClips = info.audioClips.map(url => game.resolveResource(url));
+    console.warn("preloading audio clips is not implemented yet");
+    // TODO: implement preloading audio clips
   }
 
   if (info.custom) {
