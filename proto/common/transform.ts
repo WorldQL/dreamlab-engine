6a6f3502d35a5cd02cee2d0b811fb19c9bd52f70
Jack Baron <jackmbaron@gmail.com>
2025-10-13 15:00:16 +0100

    lower almost eq threshold for angles

--- proto/common/transform.ts
+++ proto/common/transform.ts
@@ -12,7 +12,7 @@ const almostEq = (a: number, b: number, epsilon: number) => Math.abs(a - b) < ep
 export const transformsEq = (a: ITransform, b: ITransform) =>
   almostEq(a.position.x, b.position.x, 0.001) &&
   almostEq(a.position.y, b.position.y, 0.001) &&
-  almostEq(a.rotation, b.rotation, 0.1) &&
+  almostEq(a.rotation, b.rotation, 0.0001) &&
   a.scale.x === b.scale.x &&
   a.scale.y === b.scale.y &&
   a.z === b.z;
