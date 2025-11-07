b032e1fb4dd653f74dc976012a22121b03ceaae2
Jack Baron <jackmbaron@gmail.com>
2025-11-07 19:50:50 +0000

    fix projectjsonschema

--- multiplayer/server-host/web/routes/schemas.ts
+++ multiplayer/server-host/web/routes/schemas.ts
@@ -2,7 +2,10 @@ import { ProjectSchema } from "@dreamlab/scene";
 import * as z from "@dreamlab/vendor/zod.ts";
 import { Router } from "@oak/oak";
 
-const projectJsonSchema = z.toJSONSchema(ProjectSchema);
+const projectJsonSchema = z.toJSONSchema(ProjectSchema, {
+  // TODO: make sure this isnt doing anything bad
+  unrepresentable: "any",
+});
 
 export const serveSchemas = (router: Router) => {
   router.get("/schemas/project.schema.json", ctx => {
