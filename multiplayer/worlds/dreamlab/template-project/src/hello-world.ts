89e14d09a07fc3d86816ba1e57e87f2a01ea5ae3
Jackson R <1147244+Jaxkr@users.noreply.github.com>
2025-12-08 08:57:15 -0800

    Update hello-world.ts

--- multiplayer/worlds/dreamlab/template-project/src/hello-world.ts
+++ multiplayer/worlds/dreamlab/template-project/src/hello-world.ts
@@ -1,9 +1,12 @@
 import { Behavior } from "@dreamlab/engine";
 
 export default class HelloWorld extends Behavior {
+  onInitialize() {
+    console.log("hello world!");
+  }
 
-    onInitialize() {
-        console.log('hello world!');
-    }
+  onTick(): void {
+    if (!this.game.isServer()) return;
+    this.entity.transform.rotation += 0.01;
+  }
 }
-
